7763b5c95402a45ebb9874ed62930a5656b03ba4
Andrew Wanex <awanex2705@gmail.com>
2019-03-29 16:49:14 -0700

    chore: update imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
@@ -10,6 +11,13 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { UserProvider } from '../providers/user/user';
+import { RecipeProvider } from '../providers/recipe/recipe';
+import { AuthenticationProvider } from '../providers/authentication/authentication';
+import { AuthorizedInterceptor, UnauthorizedInterceptor } from '../providers/interceptor/interceptor';
+import { ProcessHttpErrorProvider } from '../providers/process-http-error/process-http-error';
+import { LibraryProvider } from '../providers/library/library';
+import { NativeStorageProvider } from '../providers/native-storage/native-storage';
 
 @NgModule({
   declarations: [
@@ -21,7 +29,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpClientModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -34,7 +43,23 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    UserProvider,
+    RecipeProvider,
+    AuthenticationProvider,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthorizedInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: UnauthorizedInterceptor,
+      multi: true
+    },
+    ProcessHttpErrorProvider,
+    LibraryProvider,
+    NativeStorageProvider
   ]
 })
 export class AppModule {}
