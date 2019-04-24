f7a824b4a42af6754ba93d752aebf838c71e923e
Andrew Wanex <awanex2705@gmail.com>
2019-04-23 17:17:37 -0700

    chore: add constants

---
+++ src/shared/constants/factors.ts
@@ -0,0 +1,8 @@
+export const ABVFactor = 131.25;
+export const BignessFactor = 1.65;
+export const BignessBase = 0.000125;
+export const BoilTimeFactor = 4.15;
+export const BoilTimeExp = -0.04;
+export const IBUFactor = 74.9;
+export const SRMFactor = 1.4922;
+export const SRMExp = 0.6859;
---
+++ src/shared/constants/shared-properties.ts
@@ -0,0 +1,5 @@
+export const sharedProperties = [
+  '_id',
+  'createdAt',
+  'updatedAt'
+];
