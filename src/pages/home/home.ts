7275ca5165a2872422d075932d8f58f9542c2be8
Andrew Wanex <awanex2705@gmail.com>
2019-04-23 17:22:08 -0700

    refactor: base home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,37 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ChangeDetectorRef, OnInit, OnDestroy } from '@angular/core';
+import { NavController, Events } from 'ionic-angular';
+
+import { UserProvider } from '../../providers/user/user';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
+  title = "Brew IO";
   private user = null;
+  activeBatches = [];
+  notifications = [];
+  private _login: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+    private cdRef: ChangeDetectorRef,
+    private events: Events,
+    private userService: UserProvider) {
+      this._login = this.loginEventHandler.bind(this);
+  }
 
+  loginEventHandler() {
+    this.user = this.userService.getUser();
+    this.cdRef.detectChanges();
+  }
+
+  ngOnDestroy() {
+    this.events.unsubscribe('on-login', this._login);
+  }
+
+  ngOnInit() {
+    this.events.subscribe('on-login', this._login);
   }
 
 }
