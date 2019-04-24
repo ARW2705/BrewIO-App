cad9b64dd8ca33bd4345ee5fa9ebf89436c0dedf
Andrew Wanex <awanex2705@gmail.com>
2019-04-23 17:09:44 -0700

    feat: add reuseable progress ring component

---
+++ src/components/progress-circle/progress-circle.ts
@@ -0,0 +1,13 @@
+import { Component, Input, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'progress-circle',
+  templateUrl: 'progress-circle.html',
+  encapsulation: ViewEncapsulation.None
+})
+export class ProgressCircleComponent {
+  @Input('settings') settings;
+
+  constructor() { }
+
+}
