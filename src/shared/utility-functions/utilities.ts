d468b86bb535f16aec9c161c28ec7e0f7231910e
Andrew Wanex <awanex2705@gmail.com>
2019-04-23 17:06:57 -0700

    feat: common utility functions

---
+++ src/shared/utility-functions/utilities.ts
@@ -0,0 +1,24 @@
+import { sharedProperties } from '../constants/shared-properties';
+
+export function clone(obj: any): any {
+  const result = JSON.parse(JSON.stringify(obj));
+  _removeShared(result);
+  return result;
+}
+
+export function getIndexById(id: string, arr: Array<any>) {
+  for (let i=0; i < arr.length; i++) {
+    if (arr[i]._id == id) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+function _removeShared(obj: any) {
+  for (const key in obj) {
+    if (sharedProperties.indexOf(key) != -1) {
+      delete obj[key];
+    }
+  }
+}
