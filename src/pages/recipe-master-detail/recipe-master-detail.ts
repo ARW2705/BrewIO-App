250c6066acf78ea009ca143245be36c646ede2f3
Andrew Wanex <awanex2705@gmail.com>
2019-04-23 17:32:31 -0700

    feat: implement crud and nav functions with proper displays

--- src/pages/recipe-master-detail/recipe-master-detail.ts
+++ src/pages/recipe-master-detail/recipe-master-detail.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Component, ChangeDetectorRef, OnInit, OnDestroy } from '@angular/core';
+import { NavController, NavParams, Events } from 'ionic-angular';
 
 import { RecipeMaster } from '../../shared/interfaces/recipe-master';
+import { Recipe } from '../../shared/interfaces/recipe';
+import { getIndexById } from '../../shared/utility-functions/utilities';
 
-import { RecipeDetailPage } from '../recipe-detail/recipe-detail';
+import { RecipeFormPage } from '../forms/recipe-form/recipe-form';
 
 import { RecipeProvider } from '../../providers/recipe/recipe';
 
@@ -11,26 +13,99 @@ import { RecipeProvider } from '../../providers/recipe/recipe';
   selector: 'page-recipe-master-detail',
   templateUrl: 'recipe-master-detail.html',
 })
-export class RecipeMasterDetailPage {
+export class RecipeMasterDetailPage implements OnInit, OnDestroy {
+  title:string = '';
   private recipeMaster: RecipeMaster = null;
   private hasActiveBatch: boolean = false;
+  private recipeIndex: number = -1;
+  private _updateMaster: any;
+  private _addRecipe: any;
+  private _updateRecipe: any;
+  private _deleteRecipe: any;
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
+    public events: Events,
+    private cdRef: ChangeDetectorRef,
     private recipeService: RecipeProvider) {
-    this.recipeMaster = this.navParams.get('master');
+      this.recipeMaster = this.navParams.get('master');
+      this.title = this.recipeMaster.name;
+      this._updateMaster = this.updateMasterEventHandler.bind(this);
+      this._addRecipe = this.addRecipeEventHandler.bind(this);
+      this._updateRecipe = this.updateRecipeEventHandler.bind(this);
+      this._deleteRecipe = this.deleteRecipeEventHandler.bind(this);
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad RecipeMasterDetailPage');
+  updateMasterEventHandler(data: any) {
+    this.recipeMaster = data;
   }
 
-  navigateToRecipeDetail(index: number) {
-    this.navCtrl.push(RecipeDetailPage, {recipe: this.recipeMaster.recipes[index]});
+  addRecipeEventHandler(data: any) {
+    this.updateSetMaster(data);
+    this.recipeMaster.recipes.push(data);
+  }
+
+  updateRecipeEventHandler(data: any) {
+    this.updateSetMaster(data);
+  }
+
+  deleteRecipeEventHandler(data: any) {
+    const toUpdate = this.recipeMaster.recipes.find(recipe => recipe._id == data.newMaster._id);
+    if (toUpdate) {
+      toUpdate.isMaster = true;
+    }
+  }
+
+  deleteRecipe(recipe: Recipe) {
+    this.recipeService.deleteRecipeById(this.recipeMaster._id, recipe._id)
+      .subscribe(response => {
+        console.log('deleted recipe');
+        const index = getIndexById(recipe._id, this.recipeMaster.recipes);
+      });
+  }
+
+  expandRecipe(index: number) {
+    this.recipeIndex = this.recipeIndex == index ? -1: index;
   }
 
   isMaster(index: number) {
     return this.recipeMaster.recipes[index]._id == this.recipeMaster.master;
+  }
+
+  navToBrewProcess(recipe: Recipe) {
+
+  }
+
+  navToRecipeForm(formType: string, recipe?: Recipe) {
+    const options = {formType: formType};
+    if (formType == 'master') {
+      options['masterData'] = this.recipeMaster;
+      options['mode'] = 'update';
+    } else if (formType == 'recipe') {
+      options['masterData'] = this.recipeMaster;
+      if (recipe) {
+        options['recipeData'] = recipe;
+        options['mode'] = 'update';
+      } else {
+        options['mode'] = 'create';
+      }
+    }
+    this.navCtrl.push(RecipeFormPage, options);
+  }
+
+  ngOnDestroy() {
+    const um = this.events.unsubscribe('update-master', this._updateMaster);
+    const ar = this.events.unsubscribe('new-recipe', this._addRecipe);
+    const ur = this.events.unsubscribe('update-recipe', this._updateRecipe);
+    const dr = this.events.unsubscribe('delete-recipe', this._deleteRecipe);
+    console.log('on destroy', um, ar, ur, dr);
+  }
+
+  ngOnInit() {
+    this.events.subscribe('update-master', this._updateMaster);
+    this.events.subscribe('new-recipe', this._addRecipe);
+    this.events.subscribe('update-recipe', this._updateRecipe);
+    this.events.subscribe('delete-recipe', this._deleteRecipe);
   }
 
   setPublic() {
@@ -39,6 +114,20 @@ export class RecipeMasterDetailPage {
       .subscribe(response => {
         this.recipeMaster.isPublic = response.isPublic;
       });
+  }
+
+  showExpandedRecipe(index: number): boolean {
+    return index == this.recipeIndex;
+  }
+
+  updateSetMaster(data: Recipe) {
+    for (let i=0; i < this.recipeMaster.recipes.length; i++) {
+      if (data._id == this.recipeMaster.recipes[i]._id) {
+        this.recipeMaster.recipes[i] = data;
+      } else if (data.isMaster) {
+        this.recipeMaster.recipes[i].isMaster = false;
+      }
+    }
   }
 
 }
