cfef032a49ce39a05a0bc57986c53f97748a8451
Andrew Wanex <awanex2705@gmail.com>
2019-06-05 11:10:45 -0700

    feat: update accordion component to use slide animation

--- src/components/accordion/accordion.ts
+++ src/components/accordion/accordion.ts
@@ -1,14 +1,34 @@
-import { Component, Input, ViewChild, ElementRef } from '@angular/core';
+import { Component, Input, ViewChild, OnChanges, SimpleChanges, ElementRef } from '@angular/core';
+
+import { slideUpDown } from '../../animations/slide';
 
 @Component({
   selector: 'accordion',
-  templateUrl: 'accordion.html'
+  templateUrl: 'accordion.html',
+  animations: [
+    slideUpDown()
+  ]
 })
-export class AccordionComponent {
-  @ViewChild('expandAccordion', {read: ElementRef}) expandAccordion;
+export class AccordionComponent implements OnChanges {
   @Input('expanded') expanded;
-  @Input('expandHeight') expandHeight;
+  @ViewChild('accordionContainer', {read: ElementRef}) container;
+  expand = {
+    value: 'collapsed',
+    params: {
+      height: 0
+    }
+  };
 
   constructor() { }
+
+  ngOnChanges(changes: SimpleChanges) {
+    const expansion = changes.expanded.currentValue;
+    this.expand = {
+      value: expansion ? 'expanded': 'collapsed',
+      params: {
+        height: this.container.nativeElement.clientHeight
+      }
+    };
+  }
 
 }
