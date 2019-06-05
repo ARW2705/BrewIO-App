b2863e70608e721424b59c5e3059cf00a6359c71
Andrew Wanex <awanex2705@gmail.com>
2019-06-05 11:15:36 -0700

    feat: add events handlers to update recipe and process values stored with user

--- src/providers/user/user.ts
+++ src/providers/user/user.ts
@@ -7,6 +7,9 @@ import 'rxjs/add/operator/catch';
 import { baseURL } from '../../shared/constants/base-url';
 import { apiVersion } from '../../shared/constants/api-version';
 import { User } from '../../shared/interfaces/user';
+import { RecipeMaster } from '../../shared/interfaces/recipe-master';
+import { Recipe } from '../../shared/interfaces/recipe';
+import { Batch } from '../../shared/interfaces/batch';
 
 import { AuthenticationProvider } from '../authentication/authentication';
 import { ProcessHttpErrorProvider } from '../process-http-error/process-http-error';
@@ -15,19 +18,68 @@ import { ProcessHttpErrorProvider } from '../process-http-error/process-http-err
 export class UserProvider {
   private loggedIn: boolean = false;
   private user: User = null;
+  private _updateMaster: any;
+  private _updateRecipe: any;
+  private _updateBatch: any;
 
   constructor(public http: HttpClient,
     private events: Events,
     private authService: AuthenticationProvider,
     private processHttpError: ProcessHttpErrorProvider) {
-    console.log('Hello UserProvider Provider');
+      this._updateMaster = this.updateMasterEventHandler.bind(this);
+      this._updateRecipe = this.updateRecipeEventHandler.bind(this);
+      this._updateBatch = this.updateBatchEventHandler.bind(this);
+      this.events.subscribe('update-master', this._updateMaster);
+      this.events.subscribe('update-recipe', this._updateRecipe);
+      this.events.subscribe('update-batch', this._updateBatch);
+  }
+
+  private updateMasterEventHandler(update: RecipeMaster) {
+    const indexToUpdate = this.user.masterList.findIndex(master => {
+      return master._id == update._id;
+    });
+    this.user.masterList[indexToUpdate] = update;
+    this.events.publish('user-update', this.user);
+  }
+
+  private updateRecipeEventHandler(update: Recipe) {
+    let indexToUpdate = -1;
+    for (let i=0; i < this.user.masterList.length; i++) {
+      indexToUpdate = this.user.masterList[i].recipes.findIndex(recipe => {
+        return recipe._id == update._id;
+      });
+      if (indexToUpdate != -1) {
+        this.user.masterList[i].recipes[indexToUpdate] = update;
+        this.events.publish('user-update', this.user);
+        return;
+      }
+    }
+  }
+
+  private updateBatchEventHandler(data: any) {
+    const indexToUpdate = this.user.inProgressList.findIndex(batch => {
+      return batch._id == data.id;
+    });
+    if (data.type == 'start') {
+      this.user.inProgressList.push(data.batchList);
+    } else if (data.type == 'next') {
+      this.user.inProgressList[indexToUpdate].currentStep++;
+    } else if (data.type == 'step-update') {
+      this.user.inProgressList[indexToUpdate] = data.update;
+    } else if (data.type == 'end') {
+      this.user.inProgressList.splice(indexToUpdate, 1);
+    }
+    this.events.publish('user-update', this.user);
   }
 
   public signUp(user: any): Observable<any> {
     return this.http.post(baseURL + apiVersion + '/users/signup', user)
       .map((response: any) => {
         if (response.success) {
-          this.logIn({username: user.username, password: user.password});
+          this.logIn({username: user.username, password: user.password})
+            .subscribe(loginResponse => {
+              console.log(loginResponse);
+            });
         }
         return response;
       })
@@ -41,6 +93,7 @@ export class UserProvider {
           this.loggedIn = true;
           return this.getUserProfile()
             .map(profile => {
+              this.user = profile;
               return profile.username;
             });
         } else {
@@ -70,6 +123,12 @@ export class UserProvider {
   public updateUserProfile(user: any): Observable<any> {
     return this.http.patch(baseURL + apiVersion + '/users/profile', user)
       .catch(error => this.processHttpError.handleError(error));
+  }
+
+  public updateUserInProgressList(list: Array<Batch>): void {
+    this.user.inProgressList = list;
+    console.log('updated list', this.user);
+    this.events.publish('user-update', this.user);
   }
 
 }
