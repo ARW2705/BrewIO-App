1b544b585b854e10330ebcbc26a108d9350276b1
Andrew Wanex <awanex2705@gmail.com>
2019-06-10 11:20:48 -0700

    feat: add methods to automatically create process steps for mash, boil, and hops addition timers. add documentation

--- src/pages/forms/recipe-form/recipe-form.ts
+++ src/pages/forms/recipe-form/recipe-form.ts
@@ -66,7 +66,125 @@ export class RecipeFormPage implements AfterViewInit {
         });
   }
 
-  constructPayload(): any {
+  /**
+   * Generate timer process step for mash or boil
+   *
+   * params: object
+   * data - form data containing mash or boil duration
+   *
+   * return: none
+  **/
+  private autoSetBoilMashDuration(data: any): void {
+    const mashIndex = this.recipe.processSchedule.findIndex(process => {
+      return process.name == 'Mash';
+    });
+    if (mashIndex == -1) {
+      // add mash timer
+      console.log('duration', data);
+      this.recipe.processSchedule.push({
+        type: 'timer',
+        name: 'Mash',
+        description: 'Mash grains',
+        duration: data.mashDuration
+      });
+    } else {
+      // update mash timer
+      for (const key in this.recipe.processSchedule[mashIndex]) {
+        if (data.hasOwnProperty(key)) {
+          this.recipe.processSchedule[mashIndex][key] == data[key];
+        }
+      }
+    }
+
+    const boilIndex = this.recipe.processSchedule.findIndex(process => {
+      return process.name == 'Boil';
+    });
+    if (boilIndex == -1) {
+      // add boil timer
+      this.recipe.processSchedule.push({
+        type: 'timer',
+        name: 'Boil',
+        description: 'Boil wort',
+        duration: data.boilDuration
+      });
+    } else {
+      // update boil timer
+      for (const key in this.recipe.processSchedule[boilIndex]) {
+        if (data.hasOwnProperty(key)) {
+          this.recipe.processSchedule[boilIndex][key] == data[key];
+        }
+      }
+    }
+  }
+
+  /**
+   * Generate timer process step for hops addition
+   *
+   * params: object
+   * data - form data containing hops addition time
+   *
+   * return: none
+  **/
+  private autoSetHopsAddition(data: any): void {
+    // remove hops timers
+    this.recipe.processSchedule = this.recipe.processSchedule.filter(process => {
+      return !process.name.match(/^(Add).*(hops)$/);
+    });
+    console.log('clear hops timers', this.recipe.processSchedule);
+
+    // add hops timers for each hops instance
+    // combine hops additions that occur at the same time
+    const hopsForTimers = this.recipe.hops.filter(hops => {
+      return !hops.dryHop;
+    });
+    console.log('gather hops instances', hopsForTimers);
+
+    hopsForTimers.sort((h1, h2) => {
+      if (h1.addAt < h2.addAt) {
+        return 1;
+      } else if (h1.addAt > h2.addAt) {
+        return -1;
+      }
+      return 0;
+    });
+
+    hopsForTimers.forEach(hopsAddition => {
+      this.recipe.processSchedule.push({
+        type: 'timer',
+        name: `Add ${hopsAddition.hopsType.name} hops`,
+        concurrent: true,
+        description: 'Hops addition',
+        duration: this.getHopsTimeRemaining(hopsAddition.addAt)
+      })
+    });
+  }
+
+  /**
+   * Automatically create timer processes for boil/mash and hops addition
+   *
+   * params: string, object
+   * type - either 'duration' for boil/mash or 'hops-addition' for hops timers
+   * data - form data containing durations to use for timer
+   *
+   * return: none
+  **/
+  private autoSetProcess(type: string, data: any): void {
+    if (type == 'hops-addition') {
+      this.autoSetHopsAddition(data);
+    } else if (type == 'duration') {
+      this.autoSetBoilMashDuration(data);
+    }
+  }
+
+  /**
+   * Format form data for server request
+   *
+   * params: none
+   *
+   * return: object
+   * - structured form data for http request
+  **/
+  private constructPayload(): any {
     let payload;
     if (this.formType == 'master') {
       if (this.docMethod == 'create') {
@@ -95,15 +213,46 @@ export class RecipeFormPage implements AfterViewInit {
         payload = this.recipe;
       }
     }
-    console.log(payload);
     return payload;
   }
 
-  getGristRatio(quantity: number): number {
+  /**
+   * Get grains quantity percentage of total
+   *
+   * params: number
+   * quantity - grain weight
+   *
+   * return: number
+   * - percentage of total quantity for given quantity
+  **/
+  private getGristRatio(quantity: number): number {
     return quantity / this.getTotalGristWeight() * 100;
   }
 
-  getIndividualIBU(hops: HopsSchedule): number {
+  /**
+   * Get time remaining after subtracting addAt time from boil time
+   *
+   * params: number
+   * addAt - time to add hops at in minutes
+   *
+   * return: number
+   * - difference between boil time and addAt time point
+  **/
+  private getHopsTimeRemaining(addAt: number): number {
+    const boilTime = this.recipe.processSchedule.find(item => item.name == 'Boil').duration;
+    return boilTime - addAt;
+  }
+
+  /**
+   * Calculate the individual IBU contribution of a hops instances
+   *
+   * params: HopsSchedule
+   * hops - an individual hops addition
+   *
+   * return: number
+   * - IBU contribution of given hops addition
+  **/
+  private getIndividualIBU(hops: HopsSchedule): number {
     return this.calculator.getIBU(
       hops.hopsType,
       hops,
@@ -113,7 +262,15 @@ export class RecipeFormPage implements AfterViewInit {
     );
   }
 
-  getTotalGristWeight(): number {
+  /**
+   * Sum total grain weight
+   *
+   * params: none
+   *
+   * return: number
+   * - sum of all recipe grain bill quantities
+  **/
+  private getTotalGristWeight(): number {
     let total = 0;
     for (let i=0; i < this.recipe.grains.length; i++) {
       total += this.recipe.grains[i].quantity;
@@ -121,16 +278,30 @@ export class RecipeFormPage implements AfterViewInit {
     return total;
   }
 
+  /**
+   * Open note modal for editing
+   *
+   * params: object
+   * options - form options to edit existing note
+   *
+   * return: none
+  **/
   private handleFormOptions(options: any): void {
     if (!options) return;
-    console.log('got options', options);
     if (options.hasOwnProperty('noteIndex')) {
-      console.log('push to index', options.noteIndex);
       this.openNoteModal('recipe', options.noteIndex);
     }
   }
 
-  isRecipeValid(): boolean {
+  /**
+   * Check if a recipe has been created - style must have been changed
+   *
+   * params: none
+   *
+   * return: boolean
+   * - true if master style id has been changed from default
+  **/
+  private isRecipeValid(): boolean {
     return this.master.style._id != defaultStyle._id;
   }
 
@@ -138,57 +309,75 @@ export class RecipeFormPage implements AfterViewInit {
     this.handleFormOptions(this.formOptions);
   }
 
-  onSubmit() {
+  /**
+   * Call appropriate HTTP request with recipe form data
+   *
+   * params: none
+   *
+   * return: none
+  **/
+  private onSubmit(): void {
     const payload = this.constructPayload();
     const message = toTitleCase(`${this.formType} ${this.docMethod} Successful!`);
     if (this.docMethod == 'create') {
       if (this.formType == 'master') {
         this.recipeService.postRecipeMaster(payload)
-          .subscribe(response => {
-            if (response) {
+          .subscribe(
+            response => {
               this.toastService.presentToast(message);
               this.navCtrl.pop();
-            } else {
-              // TODO show toast error
+            },
+            error => {
+              this.toastService.presentToast(error.error.error.message);
             }
-          });
+          );
       } else if (this.formType == 'recipe') {
         this.recipeService.postRecipeToMasterById(this.master._id, payload)
-          .subscribe(response => {
-            if (response) {
+          .subscribe(
+            response => {
               this.toastService.presentToast(message);
               this.navCtrl.pop();
-            } else {
-              // TODO show toast error
+            },
+            error => {
+              this.toastService.presentToast(error.error.error.message);
             }
-          });
+          );
       }
     } else if (this.docMethod == 'update') {
       if (this.formType == 'master') {
         this.recipeService.patchRecipeMasterById(this.master._id, payload)
-          .subscribe(response => {
-            if (response) {
+          .subscribe(
+            response => {
               this.toastService.presentToast(message);
               this.navCtrl.pop();
-            } else {
-              // TODO show toast error
+            },
+            error => {
+              this.toastService.presentToast(error.error.error.message);
             }
-          });
+          );
       } else if (this.formType == 'recipe') {
         this.recipeService.patchRecipeById(this.master._id, this.recipe._id, payload)
-          .subscribe(response => {
-            if (response) {
+          .subscribe(
+            response => {
               this.toastService.presentToast(message);
               this.navCtrl.pop();
-            } else {
-              // TODO show toast error
+            },
+            error => {
+              this.toastService.presentToast(error.error.error.message);
             }
-          });
+          );
       }
     }
   }
 
-  openGeneralModal() {
+  /**
+   * Open general recipe form modal - pass current data for update, if present
+   *
+   * params: none
+   *
+   * return: none
+  **/
+  private openGeneralModal(): void {
     const data = {
       formType: this.formType,
       mode: this.mode,
@@ -229,7 +418,14 @@ export class RecipeFormPage implements AfterViewInit {
     modal.present({keyboardClose: false});
   }
 
-  openIngredientActionSheet() {
+  /**
+   * Open ingredient form action sheet to select ingredient type to modify
+   *
+   * params: none
+   *
+   * return: none
+  **/
+  private openIngredientActionSheet(): void {
     this.actionService.openActionSheet(
       'Select an Ingredient',
       [
@@ -261,7 +457,16 @@ export class RecipeFormPage implements AfterViewInit {
     );
   }
 
-  openIngredientFormModal(type: string, toUpdate?: any) {
+  /**
+   * Open modal to create, edit, or delete specified ingredient type
+   *
+   * params: string, [object]
+   * type - the ingredient type
+   * toUpdate - current ingredient data to edit or delete
+   *
+   * return: none
+  **/
+  private openIngredientFormModal(type: string, toUpdate?: any): void {
     const data = {
       ingredientType: type,
       update: toUpdate
@@ -292,6 +497,15 @@ export class RecipeFormPage implements AfterViewInit {
     modal.present({keyboardClose: false});
   }
 
+  /**
+   * Open modal to create, edit, or delete a note
+   *
+   * params: string, [number]
+   * noteType - 'recipe' for a common note, 'batch' for a variant specific note
+   * index - the index to edit or delete
+   *
+   * return: none
+  **/
   private openNoteModal(noteType: string, index?: number): void {
     let toUpdate;
     if (index == undefined) {
@@ -331,7 +545,14 @@ export class RecipeFormPage implements AfterViewInit {
     modal.present({keyboardClose: false});
   }
 
-  openProcessActionSheet() {
+  /**
+   * Open action sheet to select the type of process step to add
+   *
+   * params: none
+   *
+   * return: none
+  **/
+  private openProcessActionSheet(): void {
     this.actionService.openActionSheet(
       'Add a process step',
       [
@@ -357,7 +578,17 @@ export class RecipeFormPage implements AfterViewInit {
     );
   }
 
-  openProcessModal(processType: string, toUpdate?: any, index?: number) {
+  /**
+   * Open modal to create, edit, or delete specified process step type
+   *
+   * params: string, [object], [number]
+   * processType - the step type, either 'manual', 'timer', or 'calendar'
+   * toUpdate - current step data to be edited or deleted
+   * index - index of step
+   *
+   * return: none
+  **/
+  private openProcessModal(processType: string, toUpdate?: any, index?: number): void {
     const options = {
       processType: toUpdate == undefined ? processType: toUpdate.type,
       update: toUpdate,
@@ -378,37 +609,63 @@ export class RecipeFormPage implements AfterViewInit {
     modal.present({keyboardClose: false});
   }
 
-  setFormTypeConfiguration(formType: string, mode: string, master: RecipeMaster, recipe: Recipe, options: any) {
-    console.log(formType, mode, master, recipe);
-    this.formType = formType;
-    this.formOptions = options;
-    this.mode = mode;
-    this.docMethod = mode;
-    if (formType == 'master') {
-      if (mode == 'create') {
-        this.title = 'Create a Recipe';
-        this.master = clone(defaultRecipeMaster);
-        this.recipe = clone(defaultRecipeMaster.recipes[0]);
+  /**
+   * Set form configuration from nav params
+   *
+   * params: string, string, RecipeMaster, Recipe, object
+   * formType - either 'master' for RecipeMaster or 'recipe' for Recipe
+   * mode - CRUD mode
+   * master - RecipeMaster instance
+   * recipe - Recipe instance
+   * options - additional configuration object
+  **/
+  private setFormTypeConfiguration(
+    formType: string,
+    mode: string,
+    master: RecipeMaster,
+    recipe: Recipe,
+    options: any
+  ): void {
+      this.formType = formType;
+      this.formOptions = options;
+      this.mode = mode;
+      this.docMethod = mode;
+      if (formType == 'master') {
+        if (mode == 'create') {
+          this.title = 'Create a Recipe';
+          this.master = clone(defaultRecipeMaster);
+          this.recipe = clone(defaultRecipeMaster.recipes[0]);
+        } else {
+          this.title = `Update ${master.name}`;
+          this.master = master;
+          this.recipe = master.recipes.find(elem => elem.isMaster);
+        }
       } else {
-        this.title = `Update ${master.name}`;
-        this.master = master;
-        this.recipe = master.recipes.find(elem => elem.isMaster);
+        if (mode == 'create') {
+          this.title = `Add Batch to ${master.name}`;
+          this.master = master;
+          this.recipe = clone(master.recipes.find(elem => elem.isMaster));
+          this.recipe.variantName = '< Add Batch Name >';
+        } else {
+          this.title = `Update ${recipe.variantName}`;
+          this.master = master;
+          this.recipe = recipe;
+        }
       }
-    } else {
-      if (mode == 'create') {
-        this.title = `Add Batch to ${master.name}`;
-        this.master = master;
-        this.recipe = clone(master.recipes.find(elem => elem.isMaster));
-        this.recipe.variantName = '< Add Batch Name >';
-      } else {
-        this.title = `Update ${recipe.variantName}`;
-        this.master = master;
-        this.recipe = recipe;
-      }
-    }
   }
 
-  sortIngredients(ingredientType: string) {
+  /**
+   * Sort ingredient array in the following orders:
+   * - grains: descending quantity
+   * - hops: chronological
+   * - yeast: descending quantity
+   *
+   * params: string
+   * ingredientType - the ingredient array to sort
+   *
+   * return: none
+  **/
+  private sortIngredients(ingredientType: string): void {
     switch(ingredientType) {
       case 'grains':
         this.recipe.grains.sort((g1, g2) => {
@@ -448,7 +705,15 @@ export class RecipeFormPage implements AfterViewInit {
     }
   }
 
-  updateDisplay(data) {
+  /**
+   * Map data to RecipeMaster and/or Recipe
+   *
+   * params: object
+   * data - contains data that may be contained in the RecipeMaster and/or Recipe
+   *
+   * return: none
+  **/
+  private updateDisplay(data: any): void {
     for (const key in data) {
       if (this.master.hasOwnProperty(key)) {
         this.master[key] = data[key];
@@ -459,7 +724,18 @@ export class RecipeFormPage implements AfterViewInit {
     }
   }
 
-  updateIngredientList(ingredient: any, type: string, toUpdate: any, deletion?: any) {
+  /**
+   * Update in memory ingredient arrays
+   *
+   * params: object, string, [object], [boolean]
+   * ingredient - ingredient data returned from ingredient form
+   * type - the ingredient type
+   * toUpdate - current ingredient data to edit
+   * deletion - true if ingredient is to be deleted
+   *
+   * return: none
+  **/
+  private updateIngredientList(ingredient: any, type: string, toUpdate?: any, deletion?: boolean): void {
     switch(type) {
       case 'grains':
         if (toUpdate) {
@@ -526,94 +802,15 @@ export class RecipeFormPage implements AfterViewInit {
     this.cdRef.detectChanges();
   }
 
-  updateRecipeValues() {
+  /**
+   * Calculate recipe values
+   *
+   * params: none
+   *
+   * return: none
+  **/
+  private updateRecipeValues(): void {
     this.calculator.calculateRecipeValues(this.recipe);
-  }
-
-  private autoSetProcess(type: string, data: any): void {
-    if (type == 'hops-addition') {
-      // remove hops timers
-      this.recipe.processSchedule = this.recipe.processSchedule.filter(process => {
-        return !process.name.match(/^(Add).*(hops)$/);
-      });
-      console.log('clear hops timers', this.recipe.processSchedule);
-
-      // add hops timers for each hops instance
-      // combine hops additions that occur at the same time
-      const hopsForTimers = this.recipe.hops.filter(hops => {
-        return !hops.dryHop;
-      });
-      console.log('gather hops instances', hopsForTimers);
-
-      hopsForTimers.sort((h1, h2) => {
-        if (h1.addAt < h2.addAt) {
-          return 1;
-        } else if (h1.addAt > h2.addAt) {
-          return -1;
-        }
-        return 0;
-      });
-
-      console.log('sort hops in order of duration', hopsForTimers);
-
-      hopsForTimers.forEach(hopsAddition => {
-        this.recipe.processSchedule.push({
-          type: 'timer',
-          name: `Add ${hopsAddition.hopsType.name} hops`,
-          concurrent: true,
-          description: 'Hops addition',
-          duration: this.getHopsTimeRemaining(hopsAddition.addAt)
-        })
-      });
-
-      console.log('add hops timers', this.recipe.processSchedule);
-    } else if (type == 'duration') {
-      const mashIndex = this.recipe.processSchedule.findIndex(process => {
-        return process.name == 'Mash';
-      });
-      if (mashIndex == -1) {
-        // add mash timer
-        console.log('duration', data);
-        this.recipe.processSchedule.push({
-          type: 'timer',
-          name: 'Mash',
-          description: 'Mash grains',
-          duration: data.mashDuration
-        });
-      } else {
-        // update mash timer
-        for (const key in this.recipe.processSchedule[mashIndex]) {
-          if (data.hasOwnProperty(key)) {
-            this.recipe.processSchedule[mashIndex][key] == data[key];
-          }
-        }
-      }
-
-      const boilIndex = this.recipe.processSchedule.findIndex(process => {
-        return process.name == 'Boil';
-      });
-      if (boilIndex == -1) {
-        // add boil timer
-        this.recipe.processSchedule.push({
-          type: 'timer',
-          name: 'Boil',
-          description: 'Boil wort',
-          duration: data.boilDuration
-        });
-      } else {
-        // update boil timer
-        for (const key in this.recipe.processSchedule[boilIndex]) {
-          if (data.hasOwnProperty(key)) {
-            this.recipe.processSchedule[boilIndex][key] == data[key];
-          }
-        }
-      }
-    }
-  }
-
-  getHopsTimeRemaining(addAt: number): number {
-    const boilTime = this.recipe.processSchedule.find(item => item.name == 'Boil').duration;
-    return boilTime - addAt;
   }
 
 }
