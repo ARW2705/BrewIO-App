8f4690dd6b60c3746449ea7d0f5b762bc7fceb33
Andrew Wanex <awanex2705@gmail.com>
2019-06-10 11:22:17 -0700

    refactor: change header to static, add documentation, reorder methods

--- src/pages/recipe-master-detail/recipe-master-detail.ts
+++ src/pages/recipe-master-detail/recipe-master-detail.ts
@@ -9,13 +9,14 @@ import { RecipeFormPage } from '../forms/recipe-form/recipe-form';
 import { ProcessPage } from '../process/process';
 
 import { RecipeProvider } from '../../providers/recipe/recipe';
+import { ToastProvider } from '../../providers/toast/toast';
 
 @Component({
   selector: 'page-recipe-master-detail',
   templateUrl: 'recipe-master-detail.html',
 })
 export class RecipeMasterDetailPage implements OnInit, OnDestroy {
-  title:string = '';
+  title: string = '';
   private recipeMaster: RecipeMaster = null;
   private hasActiveBatch: boolean = false;
   private recipeIndex: number = -1;
@@ -32,7 +33,8 @@ export class RecipeMasterDetailPage implements OnInit, OnDestroy {
     public navParams: NavParams,
     public events: Events,
     private cdRef: ChangeDetectorRef,
-    private recipeService: RecipeProvider) {
+    private recipeService: RecipeProvider,
+    private toastService: ToastProvider) {
       this.recipeMaster = this.navParams.get('master');
       this.title = this.recipeMaster.name;
       this._updateMaster = this.updateMasterEventHandler.bind(this);
@@ -41,79 +43,162 @@ export class RecipeMasterDetailPage implements OnInit, OnDestroy {
       this._deleteRecipe = this.deleteRecipeEventHandler.bind(this);
   }
 
-  updateMasterEventHandler(data: any) {
-    this.recipeMaster = data;
-  }
-
-  addRecipeEventHandler(data: any) {
+  /**
+   * Event handler for 'add-recipe' event
+   *
+   * params: Recipe
+   * data - new recipe to be added to master
+   *
+   * return: none
+  **/
+  private addRecipeEventHandler(data: Recipe): void {
     this.updateSetMaster(data);
     this.recipeMaster.recipes.push(data);
   }
 
-  updateRecipeEventHandler(data: any) {
-    this.updateSetMaster(data);
+  /**
+   * Check if a recipe can be deleted from the recipe master
+   * - must have at least one recipe at any time
+   *
+   * params: none
+   *
+   * return: boolean
+   * - true if there are at least 2 recipes present and requested recipe is not in progress
+  **/
+  private canDelete(): boolean {
+    return  this.recipeMaster.recipes.length > 1
+            && !this.deletionInProgress;
   }
 
-  deleteRecipeEventHandler(data: any) {
+  /**
+   * Delete a recipe master note
+   *
+   * params: number
+   * index - recipe master note array index to remove
+   *
+   * return: none
+  **/
+  private deleteNote(index: number): void {
+    this.recipeMaster.notes.splice(index, 1);
+    this.recipeService.patchRecipeMasterById(this.recipeMaster._id, {notes: this.recipeMaster.notes})
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
+
+  /**
+   * Delete a recipe
+   *
+   * params: Recipe
+   * recipe - recipe instance to be deleted
+   *
+   * return: none
+  **/
+  private deleteRecipe(recipe: Recipe): void {
+    this.deletionInProgress = true;
+    this.recipeService.deleteRecipeById(this.recipeMaster._id, recipe._id)
+      .subscribe(response => {
+        this.toastService.presentToast('Recipe deleted!', 1500);
+        this.deletionInProgress = false;
+        const index = getIndexById(recipe._id, this.recipeMaster.recipes);
+      });
+  }
+
+  /**
+   * Event handler for 'delete-recipe' event
+   *
+   * params: object
+   * data - contains new recipe id to be set as master
+   *
+   * return: none
+  **/
+  private deleteRecipeEventHandler(data: any): void {
     const toUpdate = this.recipeMaster.recipes.find(recipe => recipe._id == data.newMaster._id);
     if (toUpdate) {
       toUpdate.isMaster = true;
     }
   }
 
-  deleteRecipe(recipe: Recipe) {
-    this.deletionInProgress = true;
-    this.recipeService.deleteRecipeById(this.recipeMaster._id, recipe._id)
-      .subscribe(response => {
-        console.log('deleted recipe');
-        this.deletionInProgress = false;
-        const index = getIndexById(recipe._id, this.recipeMaster.recipes);
-      });
-  }
-
-  canDelete(): boolean {
-    return  this.recipeMaster.recipes.length > 1
-            && !this.deletionInProgress;
-  }
-
-  expandRecipe(index: number) {
-    this.recipeIndex = this.recipeIndex == index ? -1: index;
-  }
-
-  toggleFavorite(recipe: Recipe): void {
-    this.recipeService.patchRecipeById(
-      this.recipeMaster._id,
-      recipe._id,
-      {isFavorite: !recipe.isFavorite}
-    )
-    .subscribe(response => {
-      if (response) {
-        console.log('set fav', response);
-      }
-    })
-  }
-
-  isMaster(index: number) {
-    return this.recipeMaster.recipes[index]._id == this.recipeMaster.master;
-  }
-
-  navToBrewProcess(recipe: Recipe) {
-    this.navCtrl.push(ProcessPage, {
-      master: this.recipeMaster,
-      requestedUserId: this.recipeMaster.owner,
-      selectedRecipeId: recipe._id
-    });
-  }
-
-  expandNote(index: number): void {
+  /**
+   * Expand note at given index
+   *
+   * params: number
+   * index - note array index to expand
+   *
+   * return: none
+  **/
+  private expandNote(index: number): void {
     this.noteIndex = this.noteIndex == index ? -1: index;
   }
 
-  showExpandedNote(index: number): boolean {
-    return index == this.noteIndex;
+  /**
+   * Toggle note display and button icon
+   *
+   * params: none
+   *
+   * return: none
+  **/
+  private expandNoteMain(): void {
+    this.showNotes = !this.showNotes;
+    this.showNotesIcon = this.showNotes ? 'arrow-up': 'arrow-down';
   }
 
-  navToRecipeForm(formType: string, recipe?: Recipe, other?: any) {
+  /**
+   * Select recipe variant to expand
+   *
+   * params: number
+   * index - index for variant to expand
+   *
+   * return: none
+  **/
+  private expandRecipe(index: number): void {
+    this.recipeIndex = this.recipeIndex == index ? -1: index;
+  }
+
+  /**
+   * Check if recipe as given index is the set master
+   *
+   * params: number
+   * index - recipe array index of given recipe
+   *
+   * return: boolean
+   * - true if recipe at given index is set as the master
+  **/
+  private isMaster(index: number): boolean {
+    return this.recipeMaster.recipes[index]._id == this.recipeMaster.master;
+  }
+
+  /**
+   * Pass recipe instance to brew process page
+   *
+   * params: Recipe
+   * recipe - recipe to be used for brew process
+   *
+   * return: none
+  **/
+  private navToBrewProcess(recipe: Recipe): void {
+    if (this.recipeService.isRecipeProcessPresent(recipe)) {
+      this.navCtrl.push(ProcessPage, {
+        master: this.recipeMaster,
+        requestedUserId: this.recipeMaster.owner,
+        selectedRecipeId: recipe._id
+      });
+    } else {
+      this.toastService.presentToast('Recipe missing a process guide!', 2000);
+    }
+  }
+
+  /**
+   * Navigate to recipe form with options
+   *
+   * params: string, [Recipe], [object]
+   * formType - either 'master' for RecipeMaster or 'recipe' for Recipe
+   * recipe - recipe to update
+   * other - additional form configuration data
+   *
+   * return: none
+  **/
+  private navToRecipeForm(formType: string, recipe?: Recipe, other?: any): void {
     const options = {
       formType: formType,
       other: other
@@ -147,7 +232,14 @@ export class RecipeMasterDetailPage implements OnInit, OnDestroy {
     this.events.subscribe('delete-recipe', this._deleteRecipe);
   }
 
-  setPublic() {
+  /**
+   * Toggle recipe master public property
+   *
+   * params: none
+   *
+   * return: none
+  **/
+  private setPublic(): void {
     this.recipeService.patchRecipeMasterById(this.recipeMaster._id,
       {isPublic: !this.recipeMaster.isPublic})
       .subscribe(response => {
@@ -155,28 +247,98 @@ export class RecipeMasterDetailPage implements OnInit, OnDestroy {
       });
   }
 
-  showExpandedRecipe(index: number): boolean {
+  /**
+   * Check if note at array index should be shown
+   *
+   * params: number
+   * index - note array index to check
+   *
+   * return: boolean
+   * - true if given index is the selected index to show
+  **/
+  private showExpandedNote(index: number): boolean {
+    return index == this.noteIndex;
+  }
+
+  /**
+   * Check if recipe variant at given index should be shown
+   *
+   * params: number
+   * index - given index to check
+   *
+   * return: boolean
+   * - true if given index should be shown
+  **/
+  private showExpandedRecipe(index: number): boolean {
     return index == this.recipeIndex;
   }
 
-  deleteNote(index: number): void {
-    this.recipeMaster.notes.splice(index, 1);
-    this.recipeService.patchRecipeMasterById(this.recipeMaster._id, {notes: this.recipeMaster.notes})
-      .subscribe(response => {
-        console.log(response);
-      });
+  /**
+   * Toggle isFavorite property of recipe
+   *
+   * params: Recipe
+   * recipe - Recipe instance to modify
+   *
+   * return: none
+  **/
+  private toggleFavorite(recipe: Recipe): void {
+    this.recipeService.patchRecipeById(
+      this.recipeMaster._id,
+      recipe._id,
+      {isFavorite: !recipe.isFavorite}
+    )
+    .subscribe(response => {
+      if (response) {
+        console.log('set fav', response);
+      }
+    })
   }
 
-  expandNoteMain() {
-    this.showNotes = !this.showNotes;
-    this.showNotesIcon = this.showNotes ? 'arrow-up': 'arrow-down';
+  /**
+   * Event handler for 'update-master' event
+   *
+   * params: RecipeMaster
+   * data - updated recipe master
+   *
+   * return: none
+  **/
+  private updateMasterEventHandler(data: RecipeMaster): void {
+    this.recipeMaster = data;
   }
 
-  updateNote(index: number): void {
+  /**
+   * Navigate to recipe form to update note from array
+   *
+   * params: number
+   * index - array index to update
+   *
+   * return: none
+  **/
+  private updateNote(index: number): void {
     this.navToRecipeForm('master', null, {noteIndex: index});
   }
 
-  updateSetMaster(data: Recipe) {
+  /**
+   * Event handler for 'update-recipe' event
+   *
+   * params: Recipe
+   * data - updated recipe
+   *
+   * return: none
+  **/
+  private updateRecipeEventHandler(data: Recipe): void {
+    this.updateSetMaster(data);
+  }
+
+  /**
+   * Update recipe master list item
+   *
+   * params: Recipe
+   * data - updated recipe
+   *
+   * return: none
+  **/
+  private updateSetMaster(data: Recipe): void {
     for (let i=0; i < this.recipeMaster.recipes.length; i++) {
       if (data._id == this.recipeMaster.recipes[i]._id) {
         this.recipeMaster.recipes[i] = data;
