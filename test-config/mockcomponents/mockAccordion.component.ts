08b7692f94e246dddfe820497b256251c7a4a518
Andrew Wanex <awanex2705@gmail.com>
2019-06-20 14:36:38 -0700

    testing: add mock components

---
+++ test-config/mockcomponents/mockAccordion.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { AccordionComponent } from '../../src/components/accordion/accordion';
+
+@Component({
+  selector: 'accordion',
+  template: '<p>Mock Accordion Component</p>'
+})
+export class MockAccordionComponent { }
---
+++ test-config/mockcomponents/mockActiveBatches.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { ActiveBatchesComponent } from '../../src/components/active-batches/active-batches';
+
+@Component({
+  selector: 'active-batches',
+  template: '<p>Mock Active Batches Component</p>'
+})
+export class MockActiveBatchesComponent { }
