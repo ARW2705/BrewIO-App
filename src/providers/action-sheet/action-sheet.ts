257981b50fa9f6660295f9d76b5019854e35426a
Andrew Wanex <awanex2705@gmail.com>
2019-06-20 14:59:28 -0700

    refactor: remove method flags for AOT and reorder by name

--- src/providers/action-sheet/action-sheet.ts
+++ src/providers/action-sheet/action-sheet.ts
@@ -6,20 +6,9 @@ import { ActionSheetButton } from '../../shared/interfaces/action-sheet-buttons'
 @Injectable()
 export class ActionSheetProvider {
 
-  constructor(private actionCtrl: ActionSheetController) {
-    console.log('Hello ActionSheetProvider Provider');
-  }
+  constructor(public actionCtrl: ActionSheetController) { }
 
-  public openActionSheet(title: string, buttons: Array<ActionSheetButton>, customClass?: string): void {
-    const actionSheet = this.actionCtrl.create({
-      title: title,
-      buttons: this.generateActionSheetButtons(buttons),
-      cssClass: customClass || 'main-action-sheet'
-    })
-    actionSheet.present({keyboardClose: false});
-  }
-
-  private generateActionSheetButtons(buttons: Array<ActionSheetButton>): Array<any> {
+  generateActionSheetButtons(buttons: Array<ActionSheetButton>): Array<any> {
     const actions: Array<ActionSheetButton> = buttons.map(button => {
       const action = {
         text: button.text,
@@ -38,6 +27,15 @@ export class ActionSheetProvider {
       }
     });
     return actions;
+  }
+
+  openActionSheet(title: string, buttons: Array<ActionSheetButton>, customClass?: string): void {
+    const actionSheet = this.actionCtrl.create({
+      title: title,
+      buttons: this.generateActionSheetButtons(buttons),
+      cssClass: customClass || 'main-action-sheet'
+    })
+    actionSheet.present({keyboardClose: false});
   }
 
 }
