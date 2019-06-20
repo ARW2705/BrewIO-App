967b344f89cf81eb3554924a6bdc95fd295a40db
Andrew Wanex <awanex2705@gmail.com>
2019-06-20 15:02:53 -0700

    refactor: remove variable and method flags for AOT compiling

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,11 @@
 import { Component, ChangeDetectorRef, OnInit, OnDestroy } from '@angular/core';
 import { NavController, Events } from 'ionic-angular';
 
-import { RecipeMaster } from '../../shared/interfaces/recipe-master';
 import { Recipe } from '../../shared/interfaces/recipe';
 
 import { ProcessPage } from '../../pages/process/process';
 
 import { UserProvider } from '../../providers/user/user';
-import { RecipeProvider } from '../../providers/recipe/recipe';
 import { ModalProvider } from '../../providers/modal/modal';
 
 @Component({
@@ -15,67 +13,52 @@ import { ModalProvider } from '../../providers/modal/modal';
   templateUrl: 'home.html'
 })
 export class HomePage implements OnInit, OnDestroy {
-  private user = null;
+  user = null;
   activeBatches = [];
   notifications = [];
-  private homeTabState = 'inView';
-  private _userUpdate: any;
-  private _tabChange: any;
-  private _updateMaster: any;
+  _userUpdate: any;
+  _updateMaster: any;
+  _navUpdate: any;
 
   constructor(public navCtrl: NavController,
-    private cdRef: ChangeDetectorRef,
-    private events: Events,
-    private userService: UserProvider,
-    private recipeService: RecipeProvider,
-    private modalService: ModalProvider) {
+    public cdRef: ChangeDetectorRef,
+    public events: Events,
+    public userService: UserProvider,
+    public modalService: ModalProvider) {
       this._userUpdate = this.userUpdateEventHandler.bind(this);
-      this._tabChange = this.tabChangeEventHandler.bind(this);
+      this._navUpdate = this.navUpdateEventHandler.bind(this);
       this._updateMaster = this.updateMasterEventHandler.bind(this);
   }
 
-  ionViewDidEnter() {
-    console.log('in view');
-    this.homeTabState = 'inView';
-  }
-
-  ionViewDidLeave() {
-    console.log('out view');
-    this.homeTabState = 'outOfView';
-  }
-
-  private dismissBatch(batch: Recipe): void {
-    const toRemove = this.activeBatches.findIndex(_batch => _batch._id == batch._id);
+  dismissBatch(batch: Recipe): void {
+    const toRemove = this.activeBatches.findIndex(_batch => _batch._id === batch._id);
     this.activeBatches.splice(toRemove, 1);
     this.cdRef.detectChanges();
   }
 
-  private getBatchCurrentStep(batch: Recipe): string {
+  getBatchCurrentStep(batch: Recipe): string {
     return `${batch.processSchedule[batch.currentStep].name}`;
   }
 
-  private getBatchName(batch: Recipe): string {
+  getBatchName(batch: Recipe): string {
     return `${batch.variantName}`;
   }
 
-  private getBatchNextStep(batch: Recipe): string {
+  getBatchNextStep(batch: Recipe): string {
     return batch.currentStep < batch.processSchedule.length - 1
            ? `${batch.processSchedule[batch.currentStep + 1].name}`
            : 'Finished';
   }
 
-  private userUpdateEventHandler(data: any) {
-    if (data) {
-      this.user = data;
-    } else {
-      this.user = null;
-    }
+  userUpdateEventHandler(data: any): void {
+    console.log('incoming user update', data);
+    this.user = data ? data: null;
   }
 
-  private navToBrewProcess(batch: Recipe) {
+  navToBrewProcess(batch: Recipe): void {
     const master = this.user.masterList.find(master => {
       return master.recipes.find(recipe => {
-        return recipe._id == batch._id;
+        return recipe._id === batch._id;
       });
     });
     this.navCtrl.push(ProcessPage, {
@@ -85,28 +68,28 @@ export class HomePage implements OnInit, OnDestroy {
     });
   }
 
-  private openLogin() {
+  openLogin(): void {
     this.modalService.openLogin();
   }
 
-  private openSignup() {
+  openSignup(): void {
     this.modalService.openSignup();
   }
 
   ngOnDestroy() {
     this.events.unsubscribe('user-update', this._userUpdate);
-    this.events.unsubscribe('tab-change', this._tabChange);
+    this.events.unsubscribe('nav-update', this._navUpdate);
     this.events.unsubscribe('update-master', this._updateMaster);
   }
 
   ngOnInit() {
     this.events.subscribe('user-update', this._userUpdate);
-    this.events.subscribe('tab-change', this._tabChange);
+    this.events.subscribe('nav-update', this._navUpdate);
     this.events.subscribe('update-master', this._updateMaster);
   }
 
-  private tabChangeEventHandler(tab: any): void {
-    if (this.user && tab.dest == 'home') {
+  navUpdateEventHandler(data: any): void {
+    if (this.user && data.dest === 'home') {
       this.userService.getUserProfile()
         .subscribe(user => {
           if (user) {
@@ -116,7 +99,7 @@ export class HomePage implements OnInit, OnDestroy {
     }
   }
 
-  private updateMasterEventHandler(update: any) {
+  updateMasterEventHandler(update: any): void {
     const indexToUpdate = this.user.masterList.findIndex(master => master._id == update._id);
     this.user.masterList[indexToUpdate] = update;
   }
