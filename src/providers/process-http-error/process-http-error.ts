822fd78e2530996b5bb3deb8d4d401d9a42b3f16
Andrew Wanex <awanex2705@gmail.com>
2019-06-20 14:52:46 -0700

    refactor: add separate handling for 401 status code

--- src/providers/process-http-error/process-http-error.ts
+++ src/providers/process-http-error/process-http-error.ts
@@ -6,20 +6,22 @@ import 'rxjs/add/observable/throw';
 @Injectable()
 export class ProcessHttpErrorProvider {
 
-  constructor() {
-    console.log('Hello ProcessHttpErrorProvider Provider');
-  }
+  constructor() { }
 
+  //TODO format error messages
   handleError(error: HttpErrorResponse | any) {
     let errMsg: string;
     if (error instanceof HttpErrorResponse) {
-      console.log('handle error', error);
-      const errStatus = error.status ? error.status: 503;
-      const errText = error.status ? error.statusText: 'Service unavailable';
-      const additionalText = error.error.name == 'ValidationError'
-                             ? error.error.message
-                             : '';
-      errMsg = `<${errStatus}> ${errText || ''}: ${additionalText}`;
+      if (error.status == 401) {
+        errMsg = error.error.error.message;
+      } else {
+        const errStatus = error.status ? error.status: 503;
+        const errText = error.status ? error.statusText: 'Service unavailable';
+        const additionalText = error.error.name == 'ValidationError'
+                               ? error.error.message
+                               : '';
+        errMsg = `<${errStatus}> ${errText || ''}: ${additionalText}`;
+      }
     } else {
       errMsg = (error.message) ? error.message: error.toString();
     }
