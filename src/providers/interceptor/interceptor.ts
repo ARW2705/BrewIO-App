5199aca788d3eed73bde9e4f01d2121adcd581e5
Andrew Wanex <awanex2705@gmail.com>
2020-01-28 15:00:17 -0800

    refactor: update to reflect removal of authentication provider and moving its methods to user provider

--- src/providers/interceptor/interceptor.ts
+++ src/providers/interceptor/interceptor.ts
@@ -1,18 +1,28 @@
+/* Module imports */
 import { HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse, HttpEvent } from '@angular/common/http';
 import { Injectable, Injector } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/do';
 
-import { AuthenticationProvider } from '../authentication/authentication';
+/* Provider imports */
+import { UserProvider } from '../user/user';
 
 @Injectable()
 export class AuthorizedInterceptor implements HttpInterceptor {
 
   constructor(private injector: Injector) { }
 
+  /**
+   * Add authorization header with json web token
+   *
+   * @params: req - the outgoing http request
+   * @params: next - angular http handler to continue the request
+   *
+   * @return: observable of http event to pass response back to origin
+  **/
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const authProvider = this.injector.get(AuthenticationProvider);
-    const authToken = authProvider.getToken();
+    const userService = this.injector.get(UserProvider);
+    const authToken = userService.getToken();
     const authRequest = req.clone({headers: req.headers.set('Authorization', `bearer ${authToken}`)});
     return next.handle(authRequest);
   }
@@ -24,19 +34,27 @@ export class UnauthorizedInterceptor implements HttpInterceptor {
 
   constructor(private injector: Injector) { }
 
+  /**
+   * Handle unauthorized response
+   *
+   * @params: req - the outgoing http request
+   * @params: next - angular http handler to continue the request
+   *
+   * @return: observable of http event to pass response back to origin
+  **/
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const authProvider = this.injector.get(AuthenticationProvider);
-    const authToken = authProvider.getToken();
+    const userService = this.injector.get(UserProvider);
+    const authToken = userService.getToken();
 
     return next
       .handle(req)
       .do((event: HttpEvent<any>) => {
-        // do nothing
+        // do nothing - only handle errors
       }, (error: any) => {
         if (error instanceof HttpErrorResponse) {
           if (error.status == 401 && authToken) {
             console.log('UnauthorizedInterceptor: ', error);
-            authProvider.checkJWToken();
+            userService.checkJWToken();
           }
         }
       });
