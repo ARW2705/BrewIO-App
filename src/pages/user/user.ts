b8b8f2e1a763c6a8961c99fa228fed8527ee2b1d
Andrew Wanex <awanex2705@gmail.com>
2020-01-28 15:18:11 -0800

    refactor: update user page to use user provider's behavior subject, add documentation

--- src/pages/user/user.ts
+++ src/pages/user/user.ts
@@ -1,11 +1,18 @@
+/* Module imports */
 import { Component, ViewChild, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
-import { NavController, TextInput, NavParams, Events } from 'ionic-angular';
+import { NavController, TextInput, NavParams } from 'ionic-angular';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/takeUntil';
 
+/* Interface imports */
 import { User } from '../../shared/interfaces/user';
 
+/* Provider imports */
 import { UserProvider } from '../../providers/user/user';
 import { ModalProvider } from '../../providers/modal/modal';
+import { ToastProvider } from '../../providers/toast/toast';
 
 @Component({
   selector: 'page-user',
@@ -15,9 +22,10 @@ export class UserPage implements OnInit, OnDestroy {
   @ViewChild('email') emailField: TextInput;
   @ViewChild('firstname') firstnameField: TextInput;
   @ViewChild('lastname') lastnameField: TextInput;
+  user$: Observable<User> = null;
+  destroy$: Subject<boolean> = new Subject<boolean>();
   user: User = null;
   userForm: FormGroup = null;
-  _userUpdate: any;
   editing = '';
   originalValues = {
     email: '',
@@ -28,15 +36,40 @@ export class UserPage implements OnInit, OnDestroy {
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
     public formBuilder: FormBuilder,
-    public events: Events,
     public cdRef: ChangeDetectorRef,
     public userService: UserProvider,
-    public modalService: ModalProvider) {
-      this._userUpdate = this.userUpdateEventHandler.bind(this);
-      this.initUser();
+    public modalService: ModalProvider,
+    public toastService: ToastProvider) {
+      this.user$ = this.userService.getUser();
   }
 
-  // Select field to be edited
+  /***** Lifecycle hooks *****/
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
+
+  ngOnInit() {
+    this.user$
+      .takeUntil(this.destroy$)
+      .subscribe(user => {
+        console.log('user update in user', user);
+        this.user = user;
+        this.initForm(user);
+      });
+  }
+
+  /***** End lifecycle hooks *****/
+
+  /**
+   * Select field to be edited
+   *
+   * @params: field - the form control field to edit
+   * @params: update - new text to be applied
+   *
+   * @return: none
+  **/
   changeEdit(field: string, update: TextInput): void {
     this.editing = update === undefined ? field: '';
     if (update === undefined) {
@@ -51,14 +84,13 @@ export class UserPage implements OnInit, OnDestroy {
     }
   }
 
-  // Reset form fields to their original values
-  clearForm(): void {
-    this.originalValues.email = '';
-    this.originalValues.firstname = '';
-    this.originalValues.lastname = '';
-  }
-
-  // Profile form has a value other than original
+  /**
+   * Check if profile form has a value other than original
+   *
+   * @params: none
+   *
+   * @return: true if at least on form field has a new input value
+  **/
   hasValuesToUpdate(): boolean {
     for (const key in this.originalValues) {
       if (this.userForm.value[key] != this.originalValues[key]) {
@@ -68,67 +100,108 @@ export class UserPage implements OnInit, OnDestroy {
     return false;
   }
 
-  // Create form with profile values in form
-  initForm(): void {
+  /**
+   * Create form with profile values in form fields
+   *
+   * @params: user - user profile object
+   *
+   * @return: none
+  **/
+  initForm(user: any): void {
     this.userForm = this.formBuilder.group({
-      email: [this.user.email, [Validators.email, Validators.required]],
-      firstname: [this.user.firstname, [Validators.maxLength(25)]],
-      lastname: [this.user.lastname, [Validators.maxLength(25)]]
+      email: [(user && user.email ? user.email: ''), [Validators.email, Validators.required]],
+      firstname: [(user && user.firstname ? user.firstname: ''), [Validators.maxLength(25)]],
+      lastname: [(user && user.lastname ? user.lastname: ''), [Validators.maxLength(25)]]
     });
   }
 
-  // Get user and map profile values to form
-  initUser(): void {
-    this.user = this.userService.getUser();
-    if (this.user != null) {
-      this.originalValues.email = this.user.email;
-      this.originalValues.firstname = this.user.firstname;
-      this.originalValues.lastname = this.user.lastname;
-      this.initForm();
-    }
-  }
-
-  // Check if given field is being edited
+  /**
+   * Check if given field is being edited
+   *
+   * @params: field - form field to check
+   *
+   * @return: true if given field matches the currently editing field
+  **/
   isEditing(field: string): boolean {
     return field === this.editing;
   }
 
-  ngOnDestroy() {
-    this.events.unsubscribe('update-user', this._userUpdate);
+  /**
+   * Check if user is logged in via user provider
+   *
+   * @params: none
+   *
+   * @return: true if user is logged in
+  **/
+  isLoggedIn(): boolean {
+    return this.userService.isLoggedIn();
   }
 
-  ngOnInit() {
-    this.events.subscribe('update-user', this._userUpdate);
+  /**
+   * Update original values object with new values
+   *
+   * @params: values - key value pairs of updates to apply
+   *
+   * @return: none
+  **/
+  mapOriginalValues(values: any): void {
+    for (const key in this.originalValues) {
+      if (values.hasOwnProperty(key)) {
+        this.originalValues[key] = values[key];
+      }
+    }
   }
 
-  // Submit updated user profile
+  /**
+   * Submit updated user profile, update view on successful response or
+   * display error message on error
+   *
+   * @params: none
+   * @return: none
+  **/
   onUpdate(): void {
     this.userService.updateUserProfile(this.userForm.value)
-      .subscribe(response => {
-        this.userForm.reset();
-        this.originalValues.email = response.email;
-        this.originalValues.firstname = response.firstname;
-        this.originalValues.lastname = response.lastname;
-      });
+      .subscribe(
+        response => {
+          this.updateForm(response);
+          this.toastService.presentToast('Profile Updated');
+        },
+        error => {
+          this.toastService.presentToast(error.message);
+        }
+      );
   }
 
+  /**
+   * Open login form modal
+   *
+   * @params: none
+   * @return: none
+  **/
   openLogin(): void {
     this.modalService.openLogin();
   }
 
+  /**
+   * Open sign up form modal
+   *
+   * @params: none
+   * @return: none
+  **/
   openSignup(): void {
     this.modalService.openSignup();
   }
 
-  // 'update-user' event handler
-  userUpdateEventHandler(data: any): void {
-    if (data) {
-      this.initUser();
-      this.cdRef.detectChanges();
-    } else {
-      this.clearForm();
-      this.user = null;
-    }
+  /**
+   * Update original values and form fields with updated values
+   *
+   * @params: update - key value pairs of user profile data
+   *
+   * @return: none
+  **/
+  updateForm(update: any): void {
+    this.mapOriginalValues(update);
+    this.userForm.reset(this.originalValues);
   }
 
 }
