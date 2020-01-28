7a4c01f260c7e8cb08da987a3f47b5008a7eb8f6
Andrew Wanex <awanex2705@gmail.com>
2020-01-28 15:59:56 -0800

    refactor: update home page to use recipe and user behavior subjects, remove unused event handlers, and update docs

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,20 @@
+/* Module imports */
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NavController, Events } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/takeUntil';
 
+/* Interface imports */
 import { Recipe } from '../../shared/interfaces/recipe';
+import { User } from '../../shared/interfaces/user';
 
+/* Page imports */
 import { ProcessPage } from '../../pages/process/process';
 
+/* Provider imports */
 import { UserProvider } from '../../providers/user/user';
+import { RecipeProvider } from '../../providers/recipe/recipe';
 import { ModalProvider } from '../../providers/modal/modal';
 
 @Component({
@@ -13,77 +22,81 @@ import { ModalProvider } from '../../providers/modal/modal';
   templateUrl: 'home.html'
 })
 export class HomePage implements OnInit, OnDestroy {
+  user$: Observable<User> = null;
+  destroy$: Subject<boolean> = new Subject<boolean>();
   user = null;
   notifications = [];
-  _userUpdate: any;
-  _updateMaster: any;
-  _navUpdate: any;
 
-  constructor(public navCtrl: NavController,
+  constructor(
+    public navCtrl: NavController,
     public events: Events,
     public userService: UserProvider,
-    public modalService: ModalProvider) {
-      this._userUpdate = this.userUpdateEventHandler.bind(this);
-      this._navUpdate = this.navUpdateEventHandler.bind(this);
-      this._updateMaster = this.updateMasterEventHandler.bind(this);
+    public recipeService: RecipeProvider,
+    public modalService: ModalProvider
+  ) {
+    this.user$ = this.userService.getUser();
   }
+
+  /***** Lifecycle Hooks *****/
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
+
+  ngOnInit() {
+    this.user$
+      .takeUntil(this.destroy$)
+      .subscribe(user => {
+        this.user = user
+      });
+  }
+
+  /***** End lifecycle hooks *****/
 
   /**
    * Navigate to Process Page with required data
    *
    * @params: batch - Recipe to use as template for brew process
+   *
+   * @return: none
   **/
-  navToBrewProcess(batch: Recipe): void {
-    const master = this.user.masterList.find(master => {
-      return master.recipes.find(recipe => {
-        return recipe._id === batch._id;
+  navToBrewProcess(recipe: Recipe): void {
+    const master$ = this.recipeService.getMasterList().value
+      .find(_master$ => {
+        return _master$.value.recipes.some(_recipe => {
+          return _recipe._id === recipe._id;
+        })
       });
-    });
-    this.navCtrl.push(ProcessPage, {
-      master: master,
-      requestedUserId: master.owner,
-      selectedRecipeId: batch._id
-    });
+    const master = master$.value;
+    this.navCtrl.push(
+      ProcessPage,
+      {
+        master: master,
+        requestedUserId: master.owner,
+        selectedRecipeId: recipe._id
+      }
+    );
   }
 
+  /**
+   * Open login form modal
+   *
+   * @params: none
+   * @return: none
+  **/
   openLogin(): void {
     this.modalService.openLogin();
   }
 
+  /**
+   * Open signup form modal
+   *
+   * @params: none
+   * @return: none
+  **/
   openSignup(): void {
     this.modalService.openSignup();
-  }
-
-  navUpdateEventHandler(data: any): void {
-    if (this.user && data.dest === 'home') {
-      this.userService.getUserProfile()
-        .subscribe(user => {
-          if (user) {
-            this.user = user;
-          }
-        });
-    }
-  }
-
-  ngOnDestroy() {
-    this.events.unsubscribe('update-user', this._userUpdate);
-    this.events.unsubscribe('nav-update', this._navUpdate);
-    this.events.unsubscribe('update-master', this._updateMaster);
-  }
-
-  ngOnInit() {
-    this.events.subscribe('update-user', this._userUpdate);
-    this.events.subscribe('nav-update', this._navUpdate);
-    this.events.subscribe('update-master', this._updateMaster);
-  }
-
-  updateMasterEventHandler(update: any): void {
-    const indexToUpdate = this.user.masterList.findIndex(master => master._id == update._id);
-    this.user.masterList[indexToUpdate] = update;
-  }
-
-  userUpdateEventHandler(data: any): void {
-    this.user = data ? data: null;
   }
 
 }
