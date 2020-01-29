d70e0e32fbe8c0ea7c210dcab92ee4ad3ab5521d
Andrew Wanex <awanex2705@gmail.com>
2020-01-28 16:28:22 -0800

    refactor: update to use active batch behavior subject, update docs

--- src/components/active-batches/active-batches.ts
+++ src/components/active-batches/active-batches.ts
@@ -1,14 +1,26 @@
+/* Module imports */
 import { Component, OnInit, OnDestroy, ViewChildren, QueryList } from '@angular/core';
 import { NavController, Events, ItemSliding } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/takeUntil';
 
+/* Interface imports */
 import { RecipeMaster } from '../../shared/interfaces/recipe-master';
 import { Batch } from '../../shared/interfaces/batch';
 import { User } from '../../shared/interfaces/user';
 
+/* Utility function imports */
+import { getArrayFromObservables } from '../../shared/utility-functions/utilities';
+
+/* Page imports */
 import { ProcessPage } from '../../pages/process/process';
 
-import { UserProvider } from '../../providers/user/user';
+/* Provider imports */
+import { ProcessProvider } from '../../providers/process/process';
+import { RecipeProvider } from '../../providers/recipe/recipe';
 import { ToastProvider } from '../../providers/toast/toast';
+import { UserProvider } from '../../providers/user/user';
 
 @Component({
   selector: 'active-batches',
@@ -16,69 +28,139 @@ import { ToastProvider } from '../../providers/toast/toast';
 })
 export class ActiveBatchesComponent implements OnInit, OnDestroy {
   @ViewChildren('slidingItems') slidingItems: QueryList<ItemSliding>;
-  activeBatches: Array<Batch> = [];
+  user$: Observable<User> = null;
+  activeBatchesList$: Observable<Array<Observable<Batch>>> = null;
+  masterList$: Observable<Array<Observable<RecipeMaster>>> = null;
+  destroy$: Subject<boolean> = new Subject<boolean>();
   user: User = null;
-  _updateUser: any;
+  activeBatchesList: Array<Observable<Batch>> = [];
+  masterList: Array<Observable<RecipeMaster>> = [];
+  getArrayFromObservables = getArrayFromObservables;
   _updateHeaderNav: any;
-  _updateBatch: any;
 
   constructor(public navCtrl: NavController,
     public events: Events,
-    public userService: UserProvider,
-    public toastService: ToastProvider) {
-      this._updateUser = this.updateUserEventHandler.bind(this);
+    public processService: ProcessProvider,
+    public recipeService: RecipeProvider,
+    public toastService: ToastProvider,
+    public userService: UserProvider) {
+      this.user$ = this.userService.getUser();
+      this.activeBatchesList$ = this.processService.getActiveBatchesList();
+      this.masterList$ = this.recipeService.getMasterList();
       this._updateHeaderNav = this.updateHeaderNavEventHandler.bind(this);
-      this._updateBatch = this.updateBatchEventHandler.bind(this);
-      this.user = this.userService.getUser();
-      this.getActiveBatches();
   }
 
+  /***** Lifecycle Hooks *****/
+
   ngOnInit() {
-    this.events.subscribe('user-update', this._updateUser);
+    this.user$
+      .takeUntil(this.destroy$)
+      .subscribe(user => {
+        this.user = user;
+      });
+
+    this.activeBatchesList$
+      .takeUntil(this.destroy$)
+      .subscribe(activeBatchesList => {
+        this.activeBatchesList = activeBatchesList;
+      });
+
+    this.masterList$
+      .takeUntil(this.destroy$)
+      .subscribe(masterList => {
+        this.masterList = masterList;
+      });
+
     this.events.subscribe('update-nav-header', this._updateHeaderNav);
-    this.events.subscribe('update-batch', this._updateBatch);
   }
 
   ngOnDestroy() {
-    this.events.unsubscribe('user-update', this._updateUser);
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
     this.events.unsubscribe('update-nav-header', this._updateHeaderNav);
-    this.events.unsubscribe('update-batch', this._updateBatch);
   }
 
-  // Assign user's in progress list to active batches
-  getActiveBatches(): void {
-    this.activeBatches = this.user ? this.user.inProgressList: [];
-  }
+  /***** End lifecycle hooks *****/
 
-  // Get the variant name of the recipe associated with given batch
-  getBatchName(batch: Batch): string {
-    const master = this.getMasterByBatch(batch);
-    const recipe = master.recipes.find(recipe => recipe._id === batch.recipe);
-    return recipe.variantName;
-  }
 
-  // Get the next to be completed step's name
-  getBatchNextStep(batch: Batch): string {
+  /**
+   * Get the name of the next process step for given batch
+   *
+   * @params: batch - requested batch to get name
+   *
+   * @return: none
+  **/
+  getBatchCurrentStep(batch: Batch): string {
     return batch.schedule[batch.currentStep].name;
   }
 
+  /**
+   * Get the start datetime of the given batch
+   *
+   * @params: batch - requested batch to get start datetime
+   *
+   * @return: ISO date string
+  **/
   getBatchStartDate(batch: Batch): string {
     return batch.createdAt;
   }
 
-  // Get the recipe master of the recipe associated with given batch
+  /**
+   * Find the recipe master associated with given batch
+   *
+   * @params: batch - requested batch to use to find master
+   *
+   * @return: the recipe master or undefined if not present
+  **/
   getMasterByBatch(batch: Batch): RecipeMaster {
-    return this.user.masterList.find(master => {
-      return master.recipes.some(recipe => {
-        return recipe._id === batch.recipe;
-      });
+    const list = getArrayFromObservables(this.masterList);
+    const master = list.find(_master => {
+      return _master.recipes.some(_recipe => {
+        return _recipe._id === batch.recipe;
+      })
     });
+    return master;
   }
 
-  // Get the recipe master's name associated with given batch
-  getRecipeName(batch: Batch): string {
+  /**
+   * Get the recipe master name associated with given batch
+   *
+   * @params: batch - requested batch to use to find recipe master
+   *
+   * @return: recipe master name as string, an empty string if
+   *  recipe master is not found
+  **/
+  getRecipeMasterName(batch: Batch): string {
     const master = this.getMasterByBatch(batch);
-    return master ? master.name: 'Missing master';
+    return (master !== undefined) ? master.name: '';
+  }
+
+  /**
+   * Get the recipe variant name associated with given batch
+   *
+   * @params: batch - requested batch to use to find recipe
+   *
+   * @return: recipe variant name as string, an empty string if
+   *  recipe or master not found
+  **/
+  getRecipeName(batch: Batch): string {
+    let recipe = undefined;
+    const master = this.getMasterByBatch(batch);
+    if (master !== undefined) {
+      recipe = master.recipes.find(recipe => recipe._id === batch.recipe);
+    }
+    return (recipe !== undefined) ? recipe.variantName: '';
+  }
+
+  /**
+   * Get user login status from user service
+   *
+   * @params: none
+   *
+   * @return: true if logged in
+  **/
+  isLoggedIn(): boolean {
+    return this.userService.isLoggedIn();
   }
 
   /**
@@ -88,9 +170,11 @@ export class ActiveBatchesComponent implements OnInit, OnDestroy {
    * Pass the recipe master, the owner's id, selected recipe's id, and the selected batch id
    *
    * @params: batch - the batch instance to use in brew process
+   *
+   * @return: none
   **/
   navToBrewProcess(batch: Batch): void {
-    const master = this.getMasterByBatch(batch);
+    const master = this.getMasterByBatch(batch)
     if (master) {
       this.events.publish('update-nav-header', {
         dest: 'process',
@@ -109,25 +193,14 @@ export class ActiveBatchesComponent implements OnInit, OnDestroy {
     }
   }
 
-  // 'user-update' event handler
-  updateUserEventHandler(user: User): void {
-    this.user = user;
-    this.getActiveBatches();
-  }
-
-  // 'update-nav-header' event handler
-  updateHeaderNavEventHandler(data: any): void {
+  /**
+   * Close sliding items when navigating from current view
+   *
+   * @params: none
+   * @return: none
+  **/
+  updateHeaderNavEventHandler(): void {
     this.slidingItems.forEach(slidingItem => slidingItem.close());
-    if (data.destType === 'tab') {
-      this.getActiveBatches();
-    }
-  }
-
-  // 'update-batch' event handler
-  updateBatchEventHandler(data: any): void {
-    if (data.type === 'end') {
-      this.userService.updateUserInProgressList(data.data);
-    }
   }
 
 }
