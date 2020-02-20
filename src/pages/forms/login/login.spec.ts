13b57e03b19ba55c38f853fd30e1b4e1b22d6ef1
Andrew Wanex <awanex2705@gmail.com>
2020-02-20 14:57:52 -0800

    test: add login form tests

---
+++ src/pages/forms/login/login.spec.ts
@@ -0,0 +1,163 @@
+/* Module imports */
+import { ComponentFixture, TestBed, getTestBed, async } from '@angular/core/testing';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
+import { IonicModule, NavController, NavParams, ViewController, ToastController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+/* Constants imports */
+import { baseURL } from '../../../shared/constants/base-url';
+import { apiVersion } from '../../../shared/constants/api-version';
+
+/* Mock imports */
+import { NavMock, NavParamsMock, StorageMock, ViewControllerMock, ToastControllerMock } from '../../../../test-config/mocks-ionic';
+
+/* Page imports */
+import { LoginPage } from './login';
+
+/* Provider imports */
+import { UserProvider } from '../../../providers/user/user';
+import { ToastProvider } from '../../../providers/toast/toast';
+import { FormValidatorProvider } from '../../../providers/form-validator/form-validator';
+import { ProcessProvider } from '../../../providers/process/process';
+import { RecipeProvider } from '../../../providers/recipe/recipe';
+import { ProcessHttpErrorProvider } from '../../../providers/process-http-error/process-http-error';
+
+
+describe('Login Form', () => {
+  let fixture: ComponentFixture<LoginPage>;
+  let loginPage: LoginPage;
+  let httpMock: HttpTestingController;
+  let injector: TestBed;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        LoginPage
+      ],
+      imports: [
+        IonicModule.forRoot(LoginPage),
+        ReactiveFormsModule,
+        FormsModule,
+        HttpClientTestingModule
+      ],
+      providers: [
+        UserProvider,
+        ToastProvider,
+        ProcessProvider,
+        RecipeProvider,
+        FormValidatorProvider,
+        ProcessHttpErrorProvider,
+        { provide: NavController, useClass: NavMock },
+        { provide: NavParams, useClass: NavParamsMock },
+        { provide: ViewController, useClass: ViewControllerMock },
+        { provide: ToastController, useClass: ToastControllerMock },
+        { provide: Storage, useClass: StorageMock }
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(async(() => {
+    injector = getTestBed();
+    httpMock = injector.get(HttpTestingController);
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginPage);
+    loginPage = fixture.componentInstance;
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  test('should create component', () => {
+    fixture.detectChanges();
+    expect(loginPage).toBeDefined();
+  }); // end 'should create component' test
+
+  test('should initialize the form', () => {
+    fixture.detectChanges();
+    expect(loginPage.loginForm).toBeDefined();
+  }); // end 'should initialize the form' test
+
+  test('should be invalid when form is empty', () => {
+    fixture.detectChanges();
+    expect(loginPage.loginForm.valid).toBe(false);
+  }); // end 'should be invalid when form is empty' test
+
+  test('should close the modal', () => {
+    fixture.detectChanges();
+    const viewSpy = jest.spyOn(loginPage.viewCtrl, 'dismiss');
+    loginPage.dismiss();
+    expect(viewSpy).toHaveBeenCalledWith();
+  }); // end 'should close the modal' test
+
+  test('should have require form field error', () => {
+    fixture.detectChanges();
+    const usernameControl = loginPage.loginForm.controls.username;
+    usernameControl.markAsTouched();
+    expect(usernameControl.errors.required).toBeTruthy();
+  }); // end 'should have require form field error' test
+
+  test('should submit a login form and get a success response', done => {
+    fixture.detectChanges();
+    const formControls = loginPage.loginForm.controls;
+    formControls.username.setValue('test-user');
+    formControls.password.setValue('abcdefghij1K%');
+    expect(loginPage.loginForm.valid).toBe(true);
+    const toastSpy = jest.spyOn(loginPage.toastService, 'presentToast');
+    const viewSpy = jest.spyOn(loginPage.viewCtrl, 'dismiss');
+    loginPage.onSubmit();
+
+    const response = {
+      success: true,
+      user: {
+        username: 'test-user'
+      }
+    };
+    setTimeout(() => {
+      expect(toastSpy).toHaveBeenCalledWith(`Welcome ${response.user.username}!`, 1000, 'middle', 'bright-toast');
+      expect(viewSpy).toHaveBeenCalledWith(response);
+      done();
+    }, 100);
+
+    const loginReq = httpMock.expectOne(`${baseURL}/${apiVersion}/users/login`);
+    loginReq.flush(response);
+
+    const processReq = httpMock.expectOne(`${baseURL}/${apiVersion}/process/in-progress`);
+    processReq.flush([]);
+
+    const recipeReq = httpMock.expectOne(`${baseURL}/${apiVersion}/recipes/private/user`);
+    recipeReq.flush([]);
+  }); // end 'should submit a login form and get a success response' test
+
+  test('should submit a login form and get an error response', done => {
+    fixture.detectChanges();
+    const formControls = loginPage.loginForm.controls;
+    formControls.username.setValue('test-user');
+    formControls.password.setValue('abcdefghij1K%');
+    expect(loginPage.loginForm.valid).toBe(true);
+    const toastSpy = jest.spyOn(loginPage.toastService, 'presentToast');
+    loginPage.onSubmit();
+
+    setTimeout(() => {
+      expect(toastSpy).toHaveBeenCalledWith('<401> Username or Password is incorrect: ', 5000, 'bottom');
+      done();
+    }, 100);
+
+    const loginReq = httpMock.expectOne(`${baseURL}/${apiVersion}/users/login`);
+    loginReq.error(new ErrorEvent('Login Error'), {status: 401, statusText: 'Username or Password is incorrect'});
+  }); // end 'should submit a login form and get an error response' test
+
+  test('should toggle password visibility', () => {
+    fixture.detectChanges();
+    expect(loginPage.showPassword).toBe(false);
+    loginPage.togglePasswordVisible();
+    expect(loginPage.showPassword).toBe(true);
+    loginPage.togglePasswordVisible();
+    expect(loginPage.showPassword).toBe(false);
+  }); // end 'should toggle password visibility' test
+  
+});
