c95b59fca224979641b568ae4661a78f4212d29d
Andrew Wanex <awanex2705@gmail.com>
2020-02-21 13:42:43 -0800

    test: add ingredient form tests

---
+++ src/pages/forms/ingredient-form/ingredient-form.spec.ts
@@ -0,0 +1,350 @@
+/* Module imports */
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { IonicModule, NavController, NavParams, ViewController } from 'ionic-angular';
+
+/* Mock imports */
+import { mockGrains } from '../../../../test-config/mockmodels/mockGrains';
+import { mockHops } from '../../../../test-config/mockmodels/mockHops';
+import { mockYeast } from '../../../../test-config/mockmodels/mockYeast';
+import { mockOtherIngredient } from '../../../../test-config/mockmodels/mockOtherIngredient';
+import { NavMock, NavParamsMock, ViewControllerMock } from '../../../../test-config/mocks-ionic';
+
+/* Page imports */
+import { IngredientFormPage } from './ingredient-form';
+
+
+describe('Ingredient Form', () => {
+
+  describe('New instance', () => {
+    let fixture: ComponentFixture<IngredientFormPage>;
+    let ingredientPage: IngredientFormPage;
+
+    beforeAll(async(() => {
+      NavParamsMock.setParams('data', {
+        ingredientType: 'grains',
+        update: undefined,
+        library: mockGrains()
+      });
+    }));
+
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [
+          IngredientFormPage
+        ],
+        imports: [
+          IonicModule.forRoot(IngredientFormPage)
+        ],
+        providers: [
+          { provide: NavController, useClass: NavMock },
+          { provide: NavParams, useClass: NavParamsMock },
+          { provide: ViewController, useClass: ViewControllerMock }
+        ]
+      })
+      .compileComponents();
+    }));
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(IngredientFormPage);
+      ingredientPage = fixture.componentInstance;
+    });
+
+    test('should create the component configured to add a grains instance', () => {
+      fixture.detectChanges();
+      expect(IngredientFormPage).toBeDefined();
+      expect(ingredientPage.title).toMatch('Add grains');
+    }); // end 'should create the component configured to add a grains instance' test
+
+    test('should configure form for a grains instance', () => {
+      fixture.detectChanges();
+      expect(ingredientPage.formType).toMatch('create');
+      const form = ingredientPage.ingredientForm;
+      expect(form.controls.hasOwnProperty('mill')).toBe(true);
+    }); // end 'should configure form for a grains instance' test
+
+    test('should submit a grains instance', () => {
+      fixture.detectChanges();
+      const form = ingredientPage.ingredientForm;
+      form.controls.type.setValue(mockGrains()[0]);
+      form.controls.quantity.setValue('5');
+      form.controls.mill.setValue('0.5');
+      const viewSpy = jest.spyOn(ingredientPage.viewCtrl, 'dismiss');
+      ingredientPage.onSubmit();
+      expect(viewSpy).toHaveBeenCalledWith({
+        grainType: mockGrains()[0],
+        quantity: 5,
+        notes: [],
+        mill: 0.5
+      });
+    }); // end 'should submit a grains instance' test
+
+    test('should convert a string to a number', () => {
+      expect(ingredientPage.toNumber('1')).toBe(1);
+      expect(ingredientPage.toNumber('0.9')).toBe(0.9);
+    }); // end 'should convert a string to a number' test
+
+  }); // end 'New instance' section
+
+
+  describe('Update grains', () => {
+    let fixture: ComponentFixture<IngredientFormPage>;
+    let ingredientPage: IngredientFormPage;
+
+    beforeAll(async(() => {
+      NavParamsMock.setParams('data', {
+        ingredientType: 'grains',
+        update: {
+          grainType: mockGrains()[0],
+          quantity: 10,
+          mill: 0.25
+        },
+        library: mockGrains()
+      });
+    }));
+
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [
+          IngredientFormPage
+        ],
+        imports: [
+          IonicModule.forRoot(IngredientFormPage)
+        ],
+        providers: [
+          { provide: NavController, useClass: NavMock },
+          { provide: NavParams, useClass: NavParamsMock },
+          { provide: ViewController, useClass: ViewControllerMock }
+        ]
+      })
+      .compileComponents();
+    }));
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(IngredientFormPage);
+      ingredientPage = fixture.componentInstance;
+    });
+
+    test('should configure form to update a grains instance', () => {
+      fixture.detectChanges();
+      const form = ingredientPage.ingredientForm;
+      expect(ingredientPage.formType).toMatch('update');
+      expect(form.controls.type.value).toStrictEqual(mockGrains()[0]);
+      expect(form.controls.quantity.value).toBe(10);
+      expect(form.controls.mill.value).toBe(0.25);
+    }); // end 'should configure form to update a grains instance' test
+
+    test('should submit a grains update', () => {
+      fixture.detectChanges();
+      const viewSpy = jest.spyOn(ingredientPage.viewCtrl, 'dismiss');
+      ingredientPage.onSubmit();
+      expect(viewSpy).toHaveBeenCalledWith({
+        grainType: mockGrains()[0],
+        quantity: 10,
+        notes: [],
+        mill: 0.25
+      });
+    }); // end 'should submit a grains update' test
+
+    test('should dismiss modal with delete flag', () => {
+      fixture.detectChanges();
+      const viewSpy = jest.spyOn(ingredientPage.viewCtrl, 'dismiss');
+      ingredientPage.onDeletion();
+      expect(viewSpy).toHaveBeenCalledWith({delete: true});
+    }); // end 'should dismiss modal with delete flag' test
+
+  }); // end 'Update grains' section
+
+
+  describe('Update hops', () => {
+    let fixture: ComponentFixture<IngredientFormPage>;
+    let ingredientPage: IngredientFormPage;
+
+    beforeAll(async(() => {
+      NavParamsMock.setParams('data', {
+        ingredientType: 'hops',
+        update: {
+          hopsType: mockHops()[1],
+          quantity: 1,
+          addAt: 60,
+          dryHop: false
+        },
+        library: mockHops()
+      });
+    }));
+
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [
+          IngredientFormPage
+        ],
+        imports: [
+          IonicModule.forRoot(IngredientFormPage)
+        ],
+        providers: [
+          { provide: NavController, useClass: NavMock },
+          { provide: NavParams, useClass: NavParamsMock },
+          { provide: ViewController, useClass: ViewControllerMock }
+        ]
+      })
+      .compileComponents();
+    }));
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(IngredientFormPage);
+      ingredientPage = fixture.componentInstance;
+    });
+
+    test('should configure form to update a hops instance', () => {
+      fixture.detectChanges();
+      const form = ingredientPage.ingredientForm;
+      expect(form.controls.type.value).toStrictEqual(mockHops()[1]);
+      expect(form.controls.quantity.value).toBe(1);
+      expect(form.controls.addAt.value).toBe(60);
+      expect(form.controls.dryHop.value).toBe(false);
+    }); // end 'should configure form to update a hops instance' test
+
+    test('should submit a hops update', () => {
+      fixture.detectChanges();
+      const viewSpy = jest.spyOn(ingredientPage.viewCtrl, 'dismiss');
+      ingredientPage.onSubmit();
+      expect(viewSpy).toHaveBeenCalledWith({
+        hopsType: mockHops()[1],
+        quantity: 1,
+        addAt: 60,
+        dryHop: false,
+        notes: []
+      });
+    }); // end 'should submit a hops update' test
+
+  });// end 'Update hops' section
+
+
+  describe('Update yeast', () => {
+    let fixture: ComponentFixture<IngredientFormPage>;
+    let ingredientPage: IngredientFormPage;
+
+    beforeAll(async(() => {
+      NavParamsMock.setParams('data', {
+        ingredientType: 'yeast',
+        update: {
+          yeastType: mockYeast()[1],
+          quantity: 1,
+          requiresStarter: true
+        },
+        library: mockYeast()
+      });
+    }));
+
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [
+          IngredientFormPage
+        ],
+        imports: [
+          IonicModule.forRoot(IngredientFormPage)
+        ],
+        providers: [
+          { provide: NavController, useClass: NavMock },
+          { provide: NavParams, useClass: NavParamsMock },
+          { provide: ViewController, useClass: ViewControllerMock }
+        ]
+      })
+      .compileComponents();
+    }));
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(IngredientFormPage);
+      ingredientPage = fixture.componentInstance;
+    });
+
+    test('should configure form to update a yeast instance', () => {
+      fixture.detectChanges();
+      const form = ingredientPage.ingredientForm;
+      expect(form.controls.type.value).toStrictEqual(mockYeast()[1]);
+      expect(form.controls.quantity.value).toBe(1);
+      expect(form.controls.requiresStarter.value).toBe(true);
+    }); // end 'should configure form to update a yeast instance' test
+
+    test('should submit a yeast update', () => {
+      fixture.detectChanges();
+      const viewSpy = jest.spyOn(ingredientPage.viewCtrl, 'dismiss');
+      ingredientPage.onSubmit();
+      expect(viewSpy).toHaveBeenCalledWith({
+        yeastType: mockYeast()[1],
+        quantity: 1,
+        requiresStarter: true,
+        notes: []
+      });
+    }); // end 'should submit a yeast update' test
+
+  }); // end 'Update yeast' test
+
+
+  describe('Update other ingredient', () => {
+    let fixture: ComponentFixture<IngredientFormPage>;
+    let ingredientPage: IngredientFormPage;
+
+    beforeAll(async(() => {
+      const _mockOtherIngredient = mockOtherIngredient()[0];
+      NavParamsMock.setParams('data', {
+        ingredientType: 'otherIngredients',
+        update: {
+          type: _mockOtherIngredient.type,
+          name: _mockOtherIngredient.name,
+          description: _mockOtherIngredient.description,
+          quantity: 1,
+          units: _mockOtherIngredient.units
+        }
+      });
+    }));
+
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [
+          IngredientFormPage
+        ],
+        imports: [
+          IonicModule.forRoot(IngredientFormPage)
+        ],
+        providers: [
+          { provide: NavController, useClass: NavMock },
+          { provide: NavParams, useClass: NavParamsMock },
+          { provide: ViewController, useClass: ViewControllerMock }
+        ]
+      })
+      .compileComponents();
+    }));
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(IngredientFormPage);
+      ingredientPage = fixture.componentInstance;
+    });
+
+    test('should configure form to update an \'other\' ingredient instance', () => {
+      fixture.detectChanges();
+      const form = ingredientPage.ingredientForm;
+      const _mockOtherIngredient = mockOtherIngredient()[0];
+      expect(form.controls.type.value).toMatch(_mockOtherIngredient.type);
+      expect(form.controls.name.value).toMatch(_mockOtherIngredient.name);
+      expect(form.controls.description.value).toMatch(_mockOtherIngredient.description);
+      expect(form.controls.quantity.value).toBe(1);
+      expect(form.controls.units.value).toMatch(_mockOtherIngredient.units);
+      expect(form.controls.hasOwnProperty('notes')).toBe(false);
+    }); // end 'should configure form to update an \'other\' ingredient instance' test
+
+    test('should submit an \'other\' ingredient update', () => {
+      fixture.detectChanges();
+      const viewSpy = jest.spyOn(ingredientPage.viewCtrl, 'dismiss');
+      ingredientPage.onSubmit();
+      expect(viewSpy).toHaveBeenCalledWith({
+        name: 'other1',
+        type: 'flavor',
+        description: 'other1 description',
+        quantity: 1,
+        units: 'unit1'
+      });
+    }); // end 'should submit an \'other\' ingredient update' test
+
+  }); // end 'Update other ingredient' section
+
+});
