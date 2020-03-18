39fc1732c0d6c64fdfdeeb54b86d5a226a826f98
Andrew Wanex <awanex2705@gmail.com>
2020-03-18 12:34:26 -0700

    doc: add storage documentation

--- src/providers/storage/storage.ts
+++ src/providers/storage/storage.ts
@@ -9,7 +9,6 @@ import { _throw as throwError } from 'rxjs/observable/throw';
 /* Interface imports */
 import { User } from '../../shared/interfaces/user';
 import { RecipeMaster } from '../../shared/interfaces/recipe-master';
-// import { Process } from '../../shared/interfaces/process';
 import { Batch } from '../../shared/interfaces/batch';
 import { LibraryCache } from '../../shared/interfaces/library';
 
@@ -23,7 +22,14 @@ export class StorageProvider {
 
   constructor(public storage: Storage) { }
 
-  getProcesses(): Observable<Array<RecipeMaster>> {
+  /**
+   * Get all active batches from cache
+   *
+   * @params: none
+   *
+   * @return: Observable of array of active batches
+  **/
+  getProcesses(): Observable<Array<Batch>> {
     return fromPromise(this.storage.get(this.processStorageKey))
       .pipe(
         map((activeBatchList: string) => {
@@ -36,15 +42,35 @@ export class StorageProvider {
       );
   }
 
+  /**
+   * Remove active batches from cache
+   *
+   * @params: none
+   * @return: none
+  **/
   removeProcesses(): void {
     this.storage.remove(this.processStorageKey)
       .then(() => console.log('Active batch data cleared'));
   }
 
+  /**
+   * Store list of active batches
+   *
+   * @params: activeBatchList - list of all active batches
+   *
+   * @return: Observable of storage set response
+  **/
   setProcesses(activeBatchList: Array<Batch>): Observable<any> {
     return fromPromise(this.storage.set(this.processStorageKey, JSON.stringify(activeBatchList)));
   }
 
+  /**
+   * Get all ingredient libraries from cache
+   *
+   * @params: none
+   *
+   * @return: Observable of array of each library
+  **/
   getLibrary(): Observable<any> {
     return fromPromise(this.storage.get(this.libraryStorageKey))
       .pipe(
@@ -58,10 +84,24 @@ export class StorageProvider {
       );
   }
 
+  /**
+   * Store ingredient libraries
+   *
+   * @params: library - object containing libraries of each type
+   *
+   * @return: Observable of storage set response
+  **/
   setLibrary(library: LibraryCache): Observable<any> {
     return fromPromise(this.storage.set(this.libraryStorageKey, JSON.stringify(library)));
   }
 
+  /**
+   * Get list of recipe masters from cache
+   *
+   * @params: none
+   *
+   * @return: Observable of array of recipe masters
+  **/
   getRecipes(): Observable<Array<RecipeMaster>> {
     return fromPromise(this.storage.get(this.recipeStorageKey))
       .pipe(
@@ -75,15 +115,35 @@ export class StorageProvider {
       );
   }
 
+  /**
+   * Remove all recipe masters from cache
+   *
+   * @params: none
+   * @return: none
+  **/
   removeRecipes(): void {
     this.storage.remove(this.recipeStorageKey)
       .then(() => console.log('Recipe data cleared'));
   }
 
+  /**
+   * Store recipe masters
+   *
+   * @params: recipeMasterList - array of recipe masters
+   *
+   * @return: Observable of storage set response
+  **/
   setRecipes(recipeMasterList: Array<RecipeMaster>): Observable<any> {
     return fromPromise(this.storage.set(this.recipeStorageKey, JSON.stringify(recipeMasterList)));
   }
 
+  /**
+   * Get user data and credentials
+   *
+   * @params: none
+   *
+   * @return: Observable of user data
+  **/
   getUser(): Observable<User> {
     return fromPromise(this.storage.get(this.userStorageKey))
       .pipe(
@@ -97,11 +157,24 @@ export class StorageProvider {
       );
   }
 
+  /**
+   * Remove user data from cache
+   *
+   * @params: none
+   * @return: none
+  **/
   removeUser(): void {
     this.storage.remove(this.userStorageKey)
       .then(() => console.log('User data cleared'));
   }
 
+  /**
+   * Store user data and credentials
+   *
+   * @params: user - user profile and credentials
+   *
+   * @return: Observable of storage set response
+  **/
   setUser(user: User): Observable<any> {
     return fromPromise(this.storage.set(this.userStorageKey, JSON.stringify(user)));
   }
