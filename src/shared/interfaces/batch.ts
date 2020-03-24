8e25784b34fcb00c1cc5ec0c42a1397e48c2b6d5
Andrew Wanex <awanex2705@gmail.com>
2020-03-24 16:33:17 -0700

    refactor: update interfaces to require _id

--- src/shared/interfaces/batch.ts
+++ src/shared/interfaces/batch.ts
@@ -2,11 +2,12 @@ import { Process } from './process';
 import { Alert } from './alerts';
 
 export interface Batch {
-  _id?: string;
+  _id: string;
   createdAt?: string;
   updatedAt?: string;
+  owner: string;
   currentStep: number;
   recipe: string;
   schedule: Array<Process>;
   alerts: Array<Alert>;
-}
+};
--- src/shared/interfaces/process.ts
+++ src/shared/interfaces/process.ts
@@ -1,5 +1,5 @@
 export interface Process {
-  _id?: string;
+  _id: string;
   createdAt?: string;
   updatedAt?: string;
   type: string;
