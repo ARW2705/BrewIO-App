db8bb1bdd00232ea114e6ee5febe81e127edf0a7
Andrew Wanex <awanex2705@gmail.com>
2020-03-27 13:28:01 -0700

    refactor: change recipe master styles to pull from mockStyles

--- test-config/mockmodels/mockBatch.ts
+++ test-config/mockmodels/mockBatch.ts
@@ -5,6 +5,7 @@ import { mockProcessSchedule } from './mockProcessSchedule';
 export const mockBatch = () => {
   const mock: Batch = {
     _id: 'test-id',
+    owner: 'user-id',
     createdAt: '2020-01-01T12:00:00.000Z',
     updatedAt: '2020-02-02T08:30:00.000Z',
     currentStep: 4,
--- test-config/mockmodels/mockRecipeMasterActive.ts
+++ test-config/mockmodels/mockRecipeMasterActive.ts
@@ -2,6 +2,7 @@ import { RecipeMaster } from '../../src/shared/interfaces/recipe-master';
 
 import { mockRecipeComplete } from './mockRecipeComplete';
 import { mockRecipeIncomplete } from './mockRecipeIncomplete';
+import { mockStyles } from './mockStyles';
 
 export const mockRecipeMasterActive = () => {
   const mock: RecipeMaster = {
@@ -9,33 +10,7 @@ export const mockRecipeMasterActive = () => {
     createdAt: 'createdAt',
     updatedAt: 'updatedAt',
     name: 'active',
-    style: {
-      "originalGravity": [
-          1.048,
-          1.058
-      ],
-      "finalGravity": [
-          1.012,
-          1.018
-      ],
-      "IBU": [
-          25,
-          45
-      ],
-      "SRM": [
-          11,
-          18
-      ],
-      "co2Volume": [
-          2,
-          2.5
-      ],
-      "_id": "5ca28639f7e5f91a1f31d7cb",
-      "name": "American Amber Ale",
-      "description": "Like most amber beers, American amber ale is named after the golden to amber color this American version of English pale ale exhibits. The color is derived from the use of caramel and crystal malt additions, which are roasted to provide amber beers with the color, body and flavor many beer fans have come to appreciate. Falling under the ale beer type, amber ales ferment at warmer temperatures for what is typically a much shorter amount of time than lager style beers.",
-      "createdAt": "2019-04-01T21:44:25.987Z",
-      "updatedAt": "2019-04-01T21:44:25.987Z"
-    },
+    style: mockStyles()[0],
     notes: [],
     master: 'complete',
     owner: 'owner-id',
