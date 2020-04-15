f0fd8b87dcb4bdbb8e5ad1449cb1ab9958b0b6c3
Andrew Wanex <awanex2705@gmail.com>
2020-04-14 17:30:58 -0700

    refactor: update process provider to allow online/offline use

--- src/providers/process/process.ts
+++ src/providers/process/process.ts
@@ -27,67 +27,83 @@ import { UserProvider } from '../user/user';
 import { RecipeProvider } from '../recipe/recipe';
 import { ConnectionProvider } from '../connection/connection';
 
+
 @Injectable()
 export class ProcessProvider {
   activeBatchList$: BehaviorSubject<Array<BehaviorSubject<Batch>>> = new BehaviorSubject<Array<BehaviorSubject<Batch>>>([]);
 
-  constructor(public http: HttpClient,
+  constructor(
+    public http: HttpClient,
     public events: Events,
     public processHttpError: ProcessHttpErrorProvider,
     public storageService: StorageProvider,
     public userService: UserProvider,
     public recipeService: RecipeProvider,
-    public connectionService: ConnectionProvider) {
-      this.events.subscribe('init-data', this.initializeActiveBatchList.bind(this));
-      this.events.subscribe('clear-data', this.clearProcesses.bind(this));
+    public connectionService: ConnectionProvider
+  ) {
+    this.events.subscribe('init-data', this.initializeActiveBatchList.bind(this));
+    this.events.subscribe('clear-data', this.clearProcesses.bind(this));
   }
 
   /***** API access methods *****/
 
   /**
-   * Complete a batch
+   * Complete a batch, update database if connected and logged in
    *
    * @params: batchId - batch to end
    *
-   * @return: observable of database response of ended batch
+   * @return: observable of ended batch
   **/
   endBatchById(batchId: string): Observable<Batch> {
     if (this.connectionService.isConnected()) {
       return this.http.delete(`${baseURL}/${apiVersion}/process/in-progress/${batchId}`)
-        .flatMap(() => { return this.removeBatchFromList(batchId); })
+        .flatMap(() => {
+          return this.removeBatchFromList(batchId);
+        })
         .catch(error => this.processHttpError.handleError(error));
     }
     return this.removeBatchFromList(batchId);
   }
 
   /**
-   * Request server move process to next step, then update local active batch
+   * Go to next step of a batch's schedule, update database if connected and
+   * logged in
    *
    * @params: batchId - batch to update
+   * @params: nextIndex - the next process schedule step to go to, -1 to end the batch
    *
-   * @return: observable of updated batch response from server
+   * @return: observable of updated batch
   **/
   incrementCurrentStep(batch: Batch, nextIndex: number): Observable<Batch> {
     if (nextIndex === -1) return this.endBatchById(batch._id);
-    batch.currentStep = nextIndex;
-    const updatedBatch = this.updateBatchInList(batch);
-    if (this.connectionService.isConnected()) {
-      return this.http.patch(`${baseURL}/${apiVersion}/process/in-progress/${batch._id}`, updatedBatch)
-        .catch(error => this.processHttpError.handleError(error));
+
+    try {
+      batch.currentStep = nextIndex;
+      const updatedBatch = this.updateBatchInList(batch);
+      if (this.connectionService.isConnected()) {
+        return this.http.patch(`${baseURL}/${apiVersion}/process/in-progress/${batch._id}`, updatedBatch)
+          .catch(error => this.processHttpError.handleError(error));
+      }
+      return Observable.of(updatedBatch);
+    } catch(error) {
+      return throwError(error.message);
     }
-    return Observable.of(updatedBatch);
   }
 
   /**
-   * Http GET all active batches for user, create active batch subject, then
+   * Get all active batches for user, create active batch subject, then
    * populate active batch list
    *
-   * @params: none
+   * Get the active batches list from storage as well as request list from server.
+   * If storage is present, load with this list first to help load the app faster,
+   * or allow some functionality if offline. HTTP request the list from the
+   * server and update the activeBatchList subject when available
    *
+   * @params: none
    * @return: none
   **/
   initializeActiveBatchList(): void {
-    this.storageService.getProcesses()
+    this.storageService.getBatches()
       .subscribe(
         (activeBatchList: any) => {
           this.mapActiveBatchArrayToSubjects(activeBatchList);
@@ -97,50 +113,42 @@ export class ProcessProvider {
     if (this.connectionService.isConnected()) {
       this.http.get(`${baseURL}/${apiVersion}/process/in-progress`)
         .catch(error => this.processHttpError.handleError(error))
-        .subscribe(batchList => {
-          this.mapActiveBatchArrayToSubjects(batchList);
-          this.updateCache();
-        });
+        .subscribe(
+          batchList => {
+            this.mapActiveBatchArrayToSubjects(batchList);
+            this.updateStorage();
+          },
+          error => {
+            // TODO error feedback
+            console.log(error);
+          }
+        );
     }
   }
 
   /**
-   * Http PATCH update batch individual step
+   * Update individual batch step - update database if connected and logged in
    *
    * @params: batchId - batch id to update
    * @params: stepId - step id to update
-   * @params: update - step update to apply
+   * @params: stepUpdate - step update to apply
    *
    * @return: observable of updated batch
   **/
   patchBatchStepById(batch: Batch, stepId: string, stepUpdate: object): Observable<Batch> {
-    const updatedBatch = this.updateStepOfBatchInList(batch, stepId, stepUpdate)
-    if (this.connectionService.isConnected()) {
-      return this.http.patch<Batch>(`${baseURL}/${apiVersion}/process/in-progress/${batch._id}`, updatedBatch)
-        .map((updatedBatchResponse: Batch) => {
-          return updatedBatchResponse;
-        })
-        .catch(error => this.processHttpError.handleError(error));
+    try {
+      const updatedBatch = this.updateStepOfBatchInList(batch, stepId, stepUpdate)
+      if (this.connectionService.isConnected()) {
+        return this.http.patch<Batch>(`${baseURL}/${apiVersion}/process/in-progress/${batch._id}`, updatedBatch)
+          .map((updatedBatchResponse: Batch) => {
+            return updatedBatchResponse;
+          })
+          .catch(error => this.processHttpError.handleError(error));
+      }
+      return Observable.of(updatedBatch);
+    } catch(error) {
+      return throwError(error.message);
     }
-    return Observable.of(updatedBatch);
-  }
-
-  updateStepOfBatchInList(batch: Batch, stepId: string, stepUpdate: object): Batch {
-    const activeBatch$ = this.getActiveBatchById(batch._id);
-    const activeBatch = activeBatch$.value;
-
-    if (activeBatch.owner === null) throw throwError('Active batch is missing an owner id');
-
-    const stepIndex = activeBatch.schedule.findIndex(step => step._id === stepId);
-
-    if (stepIndex === -1) throw throwError(`Active batch missing step with id ${stepId}`);
-
-    activeBatch.alerts = activeBatch.alerts.concat(stepUpdate['alerts']);
-    activeBatch.schedule[stepIndex]['startDatetime'] = stepUpdate['startDatetime'];
-
-    activeBatch$.next(activeBatch);
-    this.updateCache();
-    return activeBatch;
   }
 
   /**
@@ -156,28 +164,45 @@ export class ProcessProvider {
     const user = this.userService.getUser().value;
     if (this.connectionService.isConnected()) {
       return this.http.get(`${baseURL}/${apiVersion}/process/user/${userId}/master/${masterRecipeId}/recipe/${recipeId}`)
-        .map((batchResponse: Batch) => {
+        .flatMap((batchResponse: Batch) => {
           return this.addBatchToList(batchResponse);
         })
         .catch(error => this.processHttpError.handleError(error));
     } else if (user._id === userId) {
       return this.generateBatchFromRecipe(masterRecipeId, recipeId, userId)
-        .map((batch: Batch) => {
+        .flatMap((batch: Batch) => {
           return this.addBatchToList(batch);
         });
     }
     return throwError('Must be connected to internet to use a public recipe');
   }
-  // startNewBatch(userId: string, masterRecipeId: string, recipeId: string): Observable<Batch> {
-  //   return this.http.get(`${baseURL}/${apiVersion}/process/user/${userId}/master/${masterRecipeId}/recipe/${recipeId}`)
-  //     .map((batchResponse: Batch) => {
-  //       this.addBatchToList(batchResponse);
-  //       return batchResponse;
-  //     })
-  //     .catch(error => this.processHttpError.handleError(error));
-  // }
 
+  /**
+   * Update a step of a batch in activeBatchList
+   *
+   * @params: batch - the batch the step belongs to
+   * @params: stepId - id of the step to update
+   * @params: stepUpdate - updated step values to apply
+   *
+   * @return the updated parent batch of the updated step
+  **/
+  updateStepOfBatchInList(batch: Batch, stepId: string, stepUpdate: object): Batch {
+    const activeBatch$ = this.getActiveBatchById(batch._id);
+    const activeBatch = activeBatch$.value;
 
+    if (activeBatch.owner === null) throw new Error('Active batch is missing an owner id');
+
+    const stepIndex = activeBatch.schedule.findIndex(step => step._id === stepId);
+
+    if (stepIndex === -1) throw new Error(`Active batch missing step with id ${stepId}`);
+
+    activeBatch.alerts = activeBatch.alerts.concat(stepUpdate['alerts']);
+    activeBatch.schedule[stepIndex]['startDatetime'] = stepUpdate['startDatetime'];
+
+    activeBatch$.next(activeBatch);
+    this.updateStorage();
+    return activeBatch;
+  }
 
   /***** End API access methods *****/
 
@@ -189,22 +214,16 @@ export class ProcessProvider {
    *
    * @params: newBatch - the new batch to create a subject of and add to list
    *
-   * @return: none
+   * @return: Observable of the new batch
   **/
-  addBatchToList(newBatch: Batch): Batch {
+  addBatchToList(newBatch: Batch): Observable<Batch> {
     const batchSubject$ = new BehaviorSubject<Batch>(newBatch);
     const batchList = this.activeBatchList$.value;
     batchList.push(batchSubject$);
     this.activeBatchList$.next(batchList);
-    this.updateCache();
-    return batchSubject$.value;
+    this.updateStorage();
+    return batchSubject$;
   }
-  // addBatchToList(newBatch: Batch): void {
-  //   const batchSubject$ = new BehaviorSubject<Batch>(newBatch);
-  //   const list = this.activeBatchList$.value;
-  //   list.push(batchSubject$);
-  //   this.activeBatchList$.next(list);
-  // }
 
   /**
    * Call complete for all recipes and clear recipeMasterList array on user logout
@@ -217,15 +236,25 @@ export class ProcessProvider {
       batch$.complete();
     });
     this.activeBatchList$.next([]);
-    this.storageService.removeProcesses();
+    this.storageService.removeBatches();
   }
 
+  /**
+   * Create a batch using a recipe process schedule as template
+   *
+   * @params: masterRecipeId - the recipe's master id
+   * @params: recipeId - the recipe id from which to copy the schedule
+   * @params: userId - the user's id (offline)
+   *
+   * @return: Observable of new batch
+  **/
   generateBatchFromRecipe(masterRecipeId: string, recipeId: string, userId: string): Observable<Batch> {
     const now = Date.now();
     return this.recipeService.getRecipeById(masterRecipeId, recipeId)
       .map((recipe: Recipe) => {
         const newBatch: Batch = {
           _id: Date.now().toString(),
+          createdAt: (new Date()).toISOString(),
           owner: userId,
           currentStep: 0,
           recipe: recipeId,
@@ -252,19 +281,10 @@ export class ProcessProvider {
    * @return: requested batch subject
   **/
   getActiveBatchById(batchId: string): BehaviorSubject<Batch> {
-    const found = this.activeBatchList$.value.find(batch$ => batch$.value._id == batchId);
-    if (found !== undefined) {
-      return found;
-    } else {
-      return new BehaviorSubject<Batch>({
-        _id: null,
-        owner: null,
-        currentStep: null,
-        recipe: null,
-        schedule: null,
-        alerts: null
-      });
-    }
+    const found = this.activeBatchList$.value.find(batch$ => batch$.value._id === batchId);
+    return  (found !== undefined)
+            ? found
+            : null;
   }
 
   /**
@@ -297,46 +317,36 @@ export class ProcessProvider {
   /**
    * Remove a batch from active batches
    *
-   * @params: toRemove - batch to remove from list
+   * @params: batchId - batch id to remove from list
    *
-   * @return: none
+   * @return: Observable of null, using for error throw/handling
   **/
   removeBatchFromList(batchId: string): Observable<Batch> {
     const batchList = this.activeBatchList$.value;
     const indexToRemove = getIndexById(batchId, getArrayFromObservables(batchList));
 
-    if (indexToRemove === -1) throw throwError(`Delete error: Active batch with id ${batchId} not found`);
+    if (indexToRemove === -1) return throwError(`Delete error: Active batch with id ${batchId} not found`);
 
     batchList[indexToRemove].complete();
     batchList.splice(indexToRemove, 1);
     this.activeBatchList$.next(batchList);
-    this.updateCache();
+    this.updateStorage();
+
     return Observable.of(null);
   }
-  // removeBatchFromList(toRemove: Batch): void {
-  //   const list = this.activeBatchList$.value;
-  //   const toRemoveIndex = list.findIndex(batch$ => batch$.value._id === toRemove._id);
-  //   if (toRemoveIndex !== -1) {
-  //     list[toRemoveIndex].complete();
-  //     list.splice(toRemoveIndex, 1);
-  //     this.activeBatchList$.next(list);
-  //   } else {
-  //     // TODO error feedback on missing batch
-  //   }
-  // }
 
   /**
    * Update a batch subject in active batch list
    *
-   * @params: updatedBatch - updated batch data
+   * @params: updatedBatch - updated batch data may be complete or partial Batch
    *
-   * @return: none
+   * @return: updated Batch
   **/
   updateBatchInList(update: Batch | object): Batch {
     const activeBatchList = this.activeBatchList$.value;
     const batchIndex = activeBatchList.findIndex(batch$ => batch$.value._id === update['_id']);
 
-    if (batchIndex === -1) throw throwError(`Active batch with id ${update['_id']} not found`);
+    if (batchIndex === -1) throw new Error(`Active batch with id ${update['_id']} not found`);
 
     const activeBatch$ = activeBatchList[batchIndex];
     const activeBatch = activeBatch$.value;
@@ -348,34 +358,26 @@ export class ProcessProvider {
 
     activeBatch$.next(activeBatch);
     this.activeBatchList$.next(activeBatchList);
-    this.updateCache();
+    this.updateStorage();
     return activeBatch;
   }
-  // updateBatchInList(updatedBatch: Batch): void {
-  //   const list = this.activeBatchList$.value;
-  //   const batchIndex = list.findIndex(batch$ => {
-  //     return batch$.value._id === updatedBatch._id
-  //   });
-  //   if (batchIndex !== -1 && updatedBatch.currentStep < updatedBatch.schedule.length) {
-  //     list[batchIndex].next(updatedBatch);
-  //     this.activeBatchList$.next(list);
-  //   } else {
-  //     // TODO error feedback on missing batch
-  //   }
-  // }
 
   /**
-   * Update the cache with current active batch list
+   * Update the storage with current active batch list
    *
    * @params: none
    * @return: none
   **/
-  updateCache(): void {
-    this.storageService.setProcesses(this.activeBatchList$.value.map((activeBatch$: BehaviorSubject<Batch>) => activeBatch$.value))
-      .subscribe(
-        () => console.log('stored active batches'),
-        (error: ErrorObservable) => console.log('active batch store error', error)
-      );
+  updateStorage(): void {
+    this.storageService.setBatches(
+      this.activeBatchList$.value.map(
+        (activeBatch$: BehaviorSubject<Batch>) => activeBatch$.value
+      )
+    )
+    .subscribe(
+      () => console.log('stored active batches'),
+      (error: ErrorObservable) => console.log('active batch store error', error)
+    );
   }
 
   /***** End utility methods *****/
