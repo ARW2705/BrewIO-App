226277e6a31ac129d2e0b335a0ff6afbf216a371
Andrew Wanex <awanex2705@gmail.com>
2020-04-22 13:27:26 -0700

    chore: remove uneeded module field

--- src/pipes/pipes.module.ts
+++ src/pipes/pipes.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { SortPipe } from './sort/sort';
 @NgModule({
 	declarations: [SortPipe],
-	imports: [],
 	exports: [SortPipe]
 })
 export class PipesModule {}
