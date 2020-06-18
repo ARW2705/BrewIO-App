091663a618f20d18779553ab1083e0306d8515b3
Andrew Wanex <awanex2705@gmail.com>
2020-06-17 18:42:41 -0700

    refactor: remove unnecessary user observable; update subcribers to pipes; implement oninit and ondestroy

--- src/pages/forms/login/login.ts
+++ src/pages/forms/login/login.ts
@@ -1,34 +1,46 @@
 /* Module imports */
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NavController, NavParams, ViewController } from 'ionic-angular';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Observable } from 'rxjs';
-
-/* Interface imports */
-import { User } from '../../../shared/interfaces/user';
+import { Subject } from 'rxjs/Subject';
+import { takeUntil } from 'rxjs/operators/takeUntil';
 
 /* Provider imports */
 import { UserProvider } from '../../../providers/user/user';
 import { ToastProvider } from '../../../providers/toast/toast';
 
+
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
 })
-export class LoginPage {
-  public loginForm: FormGroup;
-  public showPassword: boolean = false;
-  public user$: Observable<User> = null;
+export class LoginPage implements OnInit, OnDestroy {
+  loginForm: FormGroup;
+  showPassword: boolean = false;
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
     public viewCtrl: ViewController,
     public formBuilder: FormBuilder,
     public userService: UserProvider,
-    public toastService: ToastProvider) {
-      this.user$ = this.userService.getUser();
-      this.initForm();
+    public toastService: ToastProvider) { }
+
+  /***** Lifecycle Hooks *****/
+
+  ngOnInit() {
+    this.initForm();
   }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.complete();
+  }
+
+  /***** End Lifecycle Hooks *****/
+
+
+  /***** Form Methods *****/
 
   /**
    * Call ViewController dismiss method
@@ -61,14 +73,12 @@ export class LoginPage {
    * @return: none
   **/
   onSubmit() {
-    this.userService.logIn(this.loginForm.value)
+    this.userService.logIn(this.loginForm.value, false)
+      .pipe(takeUntil(this.destroy$))
       .subscribe(
         user => {
-          console.log(user);
-          if (user) {
-            this.toastService.presentToast(`Welcome ${user.username}!`, 1000, 'middle', 'bright-toast');
-            this.viewCtrl.dismiss(user);
-          }
+          this.toastService.presentToast(`Welcome ${user.username}!`, 1000, 'middle', 'bright-toast');
+          this.viewCtrl.dismiss(user);
         },
         error => {
           this.toastService.presentToast(error, 5000, 'bottom');
@@ -85,5 +95,7 @@ export class LoginPage {
   togglePasswordVisible() {
     this.showPassword = !this.showPassword;
   }
+
+  /***** End Form Methods *****/
 
 }
