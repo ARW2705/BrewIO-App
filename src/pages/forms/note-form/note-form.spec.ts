1b008f8acb6c8a80c62c1341a4f43e5acb8890f1
Andrew Wanex <awanex2705@gmail.com>
2020-06-17 18:49:51 -0700

    test: clean up test suite

--- src/pages/forms/note-form/note-form.spec.ts
+++ src/pages/forms/note-form/note-form.spec.ts
@@ -1,5 +1,5 @@
 /* Module imports */
-import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { ComponentFixture, TestBed, getTestBed, async } from '@angular/core/testing';
 import { IonicModule, NavController, NavParams, ViewController } from 'ionic-angular';
 
 /* Test configuration imports */
@@ -13,59 +13,73 @@ import { NoteFormPage } from './note-form';
 
 
 describe('Note Form', () => {
+  let fixture: ComponentFixture<NoteFormPage>;
+  let injector: TestBed;
+  let notePage: NoteFormPage;
+  let viewCtrl: ViewController;
+  configureTestBed();
+
+  beforeAll(done => (async() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        NoteFormPage
+      ],
+      imports: [
+        IonicModule.forRoot(NoteFormPage)
+      ],
+      providers: [
+        { provide: NavController, useClass: NavMock },
+        { provide: NavParams, useClass: NavParamsMock },
+        { provide: ViewController, useClass: ViewControllerMock }
+      ]
+    });
+    await TestBed.compileComponents();
+  })()
+  .then(done)
+  .catch(done.fail));
+
+  beforeEach(async(() => {
+    injector = getTestBed();
+    viewCtrl = injector.get(ViewController);
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NoteFormPage);
+    notePage = fixture.componentInstance;
+  });
 
   describe('Note creation', () => {
-    let fixture: ComponentFixture<NoteFormPage>;
-    let notePage: NoteFormPage;
-    configureTestBed();
-
     beforeAll(async(() => {
       NavParamsMock.setParams('formMethod', 'create');
       NavParamsMock.setParams('noteType', 'recipe');
     }));
 
-    beforeAll(done => (async() => {
-      TestBed.configureTestingModule({
-        declarations: [
-          NoteFormPage
-        ],
-        imports: [
-          IonicModule.forRoot(NoteFormPage)
-        ],
-        providers: [
-          { provide: NavController, useClass: NavMock },
-          { provide: NavParams, useClass: NavParamsMock },
-          { provide: ViewController, useClass: ViewControllerMock }
-        ]
-      });
-      await TestBed.compileComponents();
-    })()
-    .then(done)
-    .catch(done.fail));
-
-    beforeEach(() => {
-      fixture = TestBed.createComponent(NoteFormPage);
-      notePage = fixture.componentInstance;
-    });
-
     test('should create component', () => {
       fixture.detectChanges();
+
       expect(notePage).toBeDefined();
       expect(notePage.title).toMatch('recipe');
     }); // end 'should create component'
 
     test('should dismiss modal', () => {
       fixture.detectChanges();
-      const viewSpy = jest.spyOn(notePage.viewCtrl, 'dismiss');
+
+      const viewSpy = jest.spyOn(viewCtrl, 'dismiss');
+
       notePage.dismiss();
+
       expect(viewSpy).toHaveBeenCalled();
     }); // end 'should dimiss modal'
 
     test('should dimiss modal with note data', () => {
       fixture.detectChanges();
-      const viewSpy = jest.spyOn(notePage.viewCtrl, 'dismiss');
+
+      const viewSpy = jest.spyOn(viewCtrl, 'dismiss');
+
       notePage.note.setValue('a test note');
+
       notePage.onSubmit();
+
       expect(viewSpy).toHaveBeenCalledWith({
         method: 'create',
         note: 'a test note'
@@ -76,49 +90,25 @@ describe('Note Form', () => {
 
 
   describe('Note update', () => {
-    let fixture: ComponentFixture<NoteFormPage>;
-    let notePage: NoteFormPage;
-    configureTestBed();
-
     beforeAll(async(() => {
       NavParamsMock.setParams('formMethod', 'update');
       NavParamsMock.setParams('noteType', 'recipe');
       NavParamsMock.setParams('toUpdate', 'note to update');
     }));
 
-    beforeAll(done => (async() => {
-      TestBed.configureTestingModule({
-        declarations: [
-          NoteFormPage
-        ],
-        imports: [
-          IonicModule.forRoot(NoteFormPage)
-        ],
-        providers: [
-          { provide: NavController, useClass: NavMock },
-          { provide: NavParams, useClass: NavParamsMock },
-          { provide: ViewController, useClass: ViewControllerMock }
-        ]
-      });
-      await TestBed.compileComponents();
-    })()
-    .then(done)
-    .catch(done.fail));
-
-    beforeEach(() => {
-      fixture = TestBed.createComponent(NoteFormPage);
-      notePage = fixture.componentInstance;
-    });
-
     test('should have note content to update', () => {
       fixture.detectChanges();
+
       expect(notePage.note.value).toBe('note to update');
     }); // end 'should have note content to update' test
 
     test('should dismiss modal with delete flag', () => {
       fixture.detectChanges();
-      const viewSpy = jest.spyOn(notePage.viewCtrl, 'dismiss');
+
+      const viewSpy = jest.spyOn(viewCtrl, 'dismiss');
+
       notePage.onDelete();
+      
       expect(viewSpy).toHaveBeenCalledWith({method: 'delete'});
     }); // end 'should dismiss modal with delete flag' test
 
