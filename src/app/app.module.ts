d0f0b4426e33d9ec1537f786d8e3f22685d4dba3
Andrew Wanex <awanex2705@gmail.com>
2020-06-18 16:15:45 -0700

    chore: update imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,21 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { IonicStorageModule } from '@ionic/storage';
 import { CommonModule } from '@angular/common';
 import { Network } from '@ionic-native/network/ngx';
+import { BackgroundMode } from '@ionic-native/background-mode';
 import { ComponentsModule } from '../components/components.module';
 import { PipesModule } from '../pipes/pipes.module';
+import { UserPageModule } from '../pages/user/user.module';
+import { ProcessPageModule } from '../pages/process/process.module';
 
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { RecipePage } from '../pages/recipe/recipe';
-import { RecipeMasterDetailPage } from '../pages/recipe-master-detail/recipe-master-detail';
+import { RecipeDetailPage } from '../pages/recipe-detail/recipe-detail';
 import { RecipeFormPage } from '../pages/forms/recipe-form/recipe-form';
 import { GeneralFormPage } from '../pages/forms/general-form/general-form';
 import { ProcessFormPage } from '../pages/forms/process-form/process-form';
 import { IngredientFormPage } from '../pages/forms/ingredient-form/ingredient-form';
 import { LoginPage } from '../pages/forms/login/login';
-import { ProcessPage } from '../pages/process/process';
 import { SignupPage } from '../pages/forms/signup/signup';
 import { NoteFormPage } from '../pages/forms/note-form/note-form';
 import { InventoryPage } from '../pages/inventory/inventory';
@@ -39,11 +41,11 @@ import { ToastProvider } from '../providers/toast/toast';
 import { ActionSheetProvider } from '../providers/action-sheet/action-sheet';
 import { InventoryProvider } from '../providers/inventory/inventory';
 import { StorageProvider } from '../providers/storage/storage';
-import { SyncProvider } from '../providers/sync/sync';
 import { ConnectionProvider } from '../providers/connection/connection';
-
-import { UserPageModule } from '../pages/user/user.module';
 import { PreferencesProvider } from '../providers/preferences/preferences';
+import { SyncProvider } from '../providers/sync/sync';
+import { ClientIdProvider } from '../providers/client-id/client-id';
+import { TimerProvider } from '../providers/timer/timer';
 
 @NgModule({
   declarations: [
@@ -51,13 +53,12 @@ import { PreferencesProvider } from '../providers/preferences/preferences';
     HomePage,
     TabsPage,
     RecipePage,
-    RecipeMasterDetailPage,
+    RecipeDetailPage,
     RecipeFormPage,
     GeneralFormPage,
     IngredientFormPage,
     LoginPage,
     ProcessFormPage,
-    ProcessPage,
     SignupPage,
     NoteFormPage,
     InventoryPage
@@ -74,7 +75,8 @@ import { PreferencesProvider } from '../providers/preferences/preferences';
     ComponentsModule,
     CommonModule,
     PipesModule,
-    UserPageModule
+    UserPageModule,
+    ProcessPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -82,13 +84,12 @@ import { PreferencesProvider } from '../providers/preferences/preferences';
     HomePage,
     TabsPage,
     RecipePage,
-    RecipeMasterDetailPage,
+    RecipeDetailPage,
     RecipeFormPage,
     GeneralFormPage,
     IngredientFormPage,
     LoginPage,
     ProcessFormPage,
-    ProcessPage,
     SignupPage,
     NoteFormPage,
     InventoryPage
@@ -97,6 +98,7 @@ import { PreferencesProvider } from '../providers/preferences/preferences';
     StatusBar,
     SplashScreen,
     Network,
+    BackgroundMode,
     {
       provide: ErrorHandler,
       useClass: IonicErrorHandler
@@ -123,9 +125,11 @@ import { PreferencesProvider } from '../providers/preferences/preferences';
     ActionSheetProvider,
     InventoryProvider,
     StorageProvider,
-    SyncProvider,
     ConnectionProvider,
-    PreferencesProvider
+    PreferencesProvider,
+    SyncProvider,
+    ClientIdProvider,
+    TimerProvider
   ]
 })
 export class AppModule {}
