e04a21f80ca7d4a3e5f5408429cdb443ead3f88c
Andrew Wanex <awanex2705@gmail.com>
2020-06-19 18:19:44 -0700

    test: clean up test suite

--- src/pipes/sort/sort.spec.ts
+++ src/pipes/sort/sort.spec.ts
@@ -16,7 +16,9 @@ describe('Pipe: Sort', () => {
     const _mockAlertsArray = [mockAlertFuture(), mockAlertPast(), mockAlert()];
     const futureDatetime = _mockAlertsArray[0];
     const pastDatetime = _mockAlertsArray[1];
+
     sortPipe.transform(_mockAlertsArray, 'datetime');
+
     expect(_mockAlertsArray[0].datetime).toMatch(pastDatetime.datetime);
     expect(_mockAlertsArray[2].datetime).toMatch(futureDatetime.datetime);
   }); // end 'should sort array by datetime' test
@@ -27,7 +29,9 @@ describe('Pipe: Sort', () => {
 
   test('should not sort array without a supported sortBy type', () => {
     const _mockArray = [3, 2, 1];
+
     sortPipe.transform(_mockArray, 'number');
+
     expect(_mockArray[0]).toBe(3);
     expect(_mockArray[2]).toBe(1);
   }); // end 'should not sort array without a supported sortBy type' test
@@ -35,7 +39,9 @@ describe('Pipe: Sort', () => {
   test('should log error and not perform sorting operation with object that lacks a datetime property', () => {
     const consoleSpy = jest.spyOn(console, 'error');
     const _mockArray = [mockAlert(), {}];
+
     sortPipe.transform(_mockArray, 'datetime');
+    
     expect(consoleSpy).toHaveBeenCalledWith('Sort pipe error: comparate missing \'datetime\' property');
     expect(_mockArray[1]).toEqual({});
   }); // end 'should log error and not perform sorting operation with object that lacks a datetime property' test
