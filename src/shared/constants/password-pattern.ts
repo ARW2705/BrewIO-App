1ce67f7adc72fe21405a887ca3fe4bc1b9a6949f
Andrew Wanex <awanex2705@gmail.com>
2020-09-25 16:56:21 -0700

    refactor: placed password pattern in form validator provider

--- src/shared/constants/password-pattern.ts
+++
@@ -1,5 +0,0 @@
- // require at least one lower case letter, upper case letter, number, and special character
-export const PASSWORD_PATTERN: RegExp = RegExp(
-  '(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[!@#%&\$\^\*])',
-  'g'
-);
