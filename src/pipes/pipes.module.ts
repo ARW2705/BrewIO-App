ec0990996e993add7c962bd83e38a0153d5f1172
Andrew Wanex <awanex2705@gmail.com>
2020-09-25 17:10:30 -0700

    chore: update module imports

--- src/pipes/pipes.module.ts
+++ src/pipes/pipes.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { SortPipe } from './sort/sort';
+import { UnitConversionPipe } from './unit-conversion/unit-conversion';
+import { RatioPipe } from './ratio/ratio';
+import { CalculatePipe } from './calculate/calculate';
+import { TruncatePipe } from './truncate/truncate';
+import { RoundPipe } from './round/round';
+import { FormatStockPipe } from './format-stock/format-stock';
+import { MomentPipe } from './moment/moment';
+import { FormatTimePipe } from './format-time/format-time';
 @NgModule({
-	declarations: [SortPipe],
-	exports: [SortPipe]
+	declarations: [
+		SortPipe,
+    UnitConversionPipe,
+    RatioPipe,
+    CalculatePipe,
+    TruncatePipe,
+    RoundPipe,
+    FormatStockPipe,
+    MomentPipe,
+    FormatTimePipe
+  ],
+	exports: [
+		SortPipe,
+    UnitConversionPipe,
+    RatioPipe,
+    CalculatePipe,
+    TruncatePipe,
+    RoundPipe,
+    FormatStockPipe,
+    MomentPipe,
+    FormatTimePipe
+	]
 })
 export class PipesModule {}
